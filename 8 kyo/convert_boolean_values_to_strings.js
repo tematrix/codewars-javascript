76c458a8216d48cefdd70378c397e57912351358
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-22 16:00:05 +0300

    Convert boolean values to strings - описание

---
+++ 8 kyo/convert_boolean_values_to_strings.js
@@ -0,0 +1,8 @@
+/*
+DESCRIPTION:
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+ПЕРЕВОД:
+Завершите метод, который принимает логическое значение и возвращает строку "Да" для true или строку "Нет" для false.
+*/
