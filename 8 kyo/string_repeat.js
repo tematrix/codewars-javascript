02baa72a9e1441a2c971070bd780ace9ce560297
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-23 16:29:27 +0300

    String repeat - описание

---
+++ 8 kyo/string_repeat.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Write a function that accepts an integer n and a string s as parameters, 
+and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+
+
+ПЕРЕВОД:
+Напишите функцию, которая принимает целое число n и строки в качестве параметров 
+и возвращает строку из s, повторяющуюся ровно n раз.
+
+Примеры (ввод -> вывод)
+6, "I" -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+*/
