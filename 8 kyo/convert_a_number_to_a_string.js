c2b96e6ebaf3d141174831b69950b689525611ab
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-23 16:24:59 +0300

    Convert a Number to a String - описание

---
+++ 8 kyo/convert_a_number_to_a_string.js
@@ -0,0 +1,23 @@
+/*
+DESCRIPTION:
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+
+
+ПЕРЕВОД:
+Нам нужна функция, которая может преобразовать число (integer) в строку.
+
+Какие способы достижения этого вы знаете?
+
+Примеры (ввод -> вывод):
+123 --> "123"
+999 --> "999"
+-100 --> "-100"
+*/
+
