8daaf3a285fadf0bcdf315491c90aea9692de563
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-23 16:35:32 +0300

    Remove String Spaces - описание

---
+++ 8 kyo/remove_string_spaces.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+Write a function that removes the spaces from the string, then return the resultant string.
+
+Examples:
+
+Input -> Output
+"8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+"8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+"8aaaaa dddd r     " -> "8aaaaaddddr"
+
+
+ПЕРЕВОД:
+Напишите функцию, которая удаляет пробелы из строки, затем возвращает результирующую строку.
+
+Примеры:
+
+Ввод -> Вывод
+"8 j 8 mBliB8g imjB8B8 jl B" -> "8j8mBliB8gimjB8B8jlB"
+""8 8 Bi fk8h B 8 BB8B B B B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+"8aaaaa dddd r " -> "8aaaaaddddr"
+*/
