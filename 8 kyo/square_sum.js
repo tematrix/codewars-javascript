b88af66b11bf83524001bae646e1076c2a149738
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-25 11:07:16 +0300

    Square Sum - description

---
+++ 8 kyo/square_sum.js
@@ -0,0 +1,13 @@
+/*
+DESCRIPTION:
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 
+1^2+2^2+2^2=9
+
+ПЕРЕВОД:
+Завершите функцию квадратной суммы так, чтобы она возводила в квадрат каждое переданное в нее число, а затем суммировала результаты.
+
+Например, для [1, 2, 2] он должен возвращать 9
+1^2+2^2+2^2=9
+*/
