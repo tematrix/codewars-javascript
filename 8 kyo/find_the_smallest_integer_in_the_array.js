68be4d5ec36da36d466421ea903c8c4329927d6a
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-25 11:11:32 +0300

    Find the smallest integer in the array - description

---
+++ 8 kyo/find_the_smallest_integer_in_the_array.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+
+ПЕРЕВОД:
+Учитывая массив целых чисел, ваше решение должно найти наименьшее целое число.
+
+Например:
+Учитывая [34, 15, 88, 2], ваше решение вернет 2
+Учитывая [34, -345, -1, 100], ваше решение вернет -345
+Вы можете предположить, для целей этого ката, что предоставленный массив не будет пустым.
+*/
