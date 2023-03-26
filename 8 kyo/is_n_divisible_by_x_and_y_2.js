07b6444c1bf04ce39adcac822ab4e480f9b37ddb
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-26 12:41:51 +0300

    Is n divisible by x and y (ver2) - decision

--- 8 kyo/is_n_divisible_by_x_and_y_2.js
+++ 8 kyo/is_n_divisible_by_x_and_y_2.js
@@ -20,3 +20,12 @@ Examples:
 3) n = 100, x = 5, y = 3 => ложно, потому что 100 не делится на 3
 4) n = 12, x = 7, y = 5 => ложно, потому что 12 не делится ни на 7, ни на 5
 */
+
+function isDivisible(n, x, y) {
+    return n % x==0 && n % y ==0;
+}
+
+console.log(isDivisible(3,1,3));
+console.log(isDivisible(12,2,6));
+console.log(isDivisible(100,5,3));
+console.log(isDivisible(12,7,5));
