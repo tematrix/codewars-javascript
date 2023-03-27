a919ab9db3db94441ff0b22ac0e17914f7483322
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-27 14:12:08 +0300

    Vowel count - description

---
+++ 7 kyo/vowel_count.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+
+ПЕРЕВОД:
+Возвращает количество гласных в данной строке.
+
+Мы будем рассматривать a, e, i, o, u как гласные для этого ката (но не y).
+
+Входная строка будет состоять только из строчных букв и/или пробелов.
+*/
