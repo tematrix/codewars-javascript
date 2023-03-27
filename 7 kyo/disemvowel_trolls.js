ca7c20b8c25d8458e44279e71472b3086c813290
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-27 14:15:59 +0300

    Disemvowel trolls - description

---
+++ 7 kyo/disemvowel_trolls.js
@@ -0,0 +1,27 @@
+/*
+DESCRIPTION:
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, 
+neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+
+
+ПЕРЕВОД:
+Тролли атакуют ваш раздел комментариев!
+
+Распространенный способ справиться с этой ситуацией - удалить все гласные из комментариев троллей, 
+нейтрализуя угрозу.
+
+Ваша задача - написать функцию, которая принимает строку и возвращает новую строку с 
+удаленными всеми гласными.
+
+Например, строка "Этот веб-сайт для неудачников, LOL!" стала бы "This wbst s fr lsrs LL!".
+
+Примечание: в этом ката y не считается гласной.
+*/
