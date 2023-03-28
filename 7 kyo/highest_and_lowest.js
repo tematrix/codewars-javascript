0e5984900489da5bdfe27716b327b1340d647f53
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-28 12:02:54 +0300

    Highest and lowest - description

---
+++ 7 kyo/highest_and_lowest.js
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION:
+In this little assignment you are given a string of space separated numbers, 
+and have to return the highest and lowest number.
+
+Examples
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+Notes:
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.
+
+
+ПЕРЕВОД:
+В этом небольшом задании вам дается строка чисел, разделенных пробелами, 
+и вы должны вернуть самое высокое и самое низкое число.
+
+Примеры
+высокого и Низкого("1 2 3 4 5"); // вернуть "5 1"
+высокий и Низкий("1 2 -3 4 5"); // возврат "5 -3"
+высокий и Низкий("1 9 3 4 -5"); // вернуть "9 -5"
+
+Записи:
+Все числа действительны в формате Int32, проверять их не нужно.
+Во входной строке всегда будет хотя бы одно число.
+Выходная строка должна состоять из двух чисел, разделенных одним пробелом, причем наибольшее число должно быть первым.
+*/
