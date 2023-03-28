958de628cdb97de190962c015e743706f623eb90
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-28 12:07:42 +0300

    Descending order - description

---
+++ 7 kyo/descending_order.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+Your task is to make a function that can take any non-negative integer as an argument 
+and return it with its digits in descending order. Essentially, rearrange the digits 
+to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+
+
+ПЕРЕВОД:
+Ваша задача - создать функцию, которая может принимать любое неотрицательное целое число 
+в качестве аргумента и возвращать его с его цифрами в порядке убывания. 
+По сути, переставьте цифры так, чтобы получилось максимально возможное число.
+
+Примеры:
+Вход: 42145 Выход: 54421
+
+Вход: 145263 Выход: 654321
+
+Вход: 123456789 Выход: 987654321
+*/
