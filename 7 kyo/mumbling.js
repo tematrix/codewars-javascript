017fad404ad9f0115b85fb37b2a9b64cad678491
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-28 12:11:52 +0300

    Mumbling - description

---
+++ 7 kyo/mumbling.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
+ПЕРЕВОД:
+На этот раз никакой истории, никакой теории. Приведенные ниже примеры показывают вам, как написать функцию accum:
+
+Примеры:
+накопление("abcd") -> "A-Bb-Ccc-Dddd"
+накопление("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Гггг"
+накопление("cwAt") -> "C-Ww-Aaa-Tttt"
+
+Параметр accum - это строка, которая включает в себя только буквы из a..z и A..Z.
+*/
