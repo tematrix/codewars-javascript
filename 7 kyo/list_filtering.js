5662501a39f348e7954383d5c3326bfa8f4cef09
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-29 12:35:13 +0300

    List filtering - decision

--- 7 kyo/list_filtering.js
+++ 7 kyo/list_filtering.js
@@ -18,3 +18,13 @@ filter_list([1,2,'a','b']) == [1,2]
 filter_list([1,'a','b',0,15]) == [1,0,15]
 filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 */
+
+function filterList(l) {
+    let a = [];
+    l.forEach(i => {if(typeof i == 'number') {a.push(i);}});
+    return a; 
+}
+
+console.log(filterList([1,2,'a','b']));
+console.log(filterList([1,'a','b',0,15]));
+console.log(filterList([1,2,'aasf','1','123',123]));
