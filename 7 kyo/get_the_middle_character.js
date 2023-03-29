31dbe79f510727591223a71bb46ace1dfb707d51
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-29 12:23:02 +0300

    Get the middle character - description

---
+++ 7 kyo/get_the_middle_character.js
@@ -0,0 +1,52 @@
+/*
+DESCRIPTION:
+You are going to be given a word. Your job is to return the middle character of the word. 
+If the word's length is odd, return the middle character. 
+If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+#Input
+
+A word (string) of length 0 < str < 1000 (In javascript you may get 
+slightly more than 1000 in some test cases due to an error in the test cases). 
+You do not need to test for this. This is only here to tell you 
+that you do not need to worry about your solution timing out.
+
+#Output
+
+The middle character(s) of the word represented as a string.
+
+
+ПЕРЕВОД:
+Вам будет предоставлено слово. Ваша задача - вернуть средний символ слова. 
+Если длина слова нечетная, верните средний символ. 
+Если длина слова четная, верните 2 средних символа.
+
+#Примеры:
+
+Kata.getMiddle("тест") должен возвращать "es"
+
+Kata.getMiddle("тестирование") должен возвращать "t"
+
+Kata.getMiddle("middle") должен возвращать "dd"
+
+Kata.getMiddle("A") должен возвращать "A"
+#Ввод
+
+Слово (строка) длиной 0 < str < 1000 (в javascript вы можете получить 
+немного больше 1000 в некоторых тестовых примерах из-за ошибки в тестовых примерах). 
+Вам не нужно проводить тестирование для этого. Это сделано только для того, 
+чтобы сказать вам, что вам не нужно беспокоиться о том, что время ожидания вашего решения истекло.
+
+#Вывод
+
+Средний символ(ы) слова, представленного в виде строки.
+*/
