edaceeaf8599ba71ed4d38f30a05825cebb5c2bd
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-29 12:33:18 +0300

    List filtering - description

---
+++ 7 kyo/list_filtering.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+In this kata you will create a function that takes a list of non-negative integers 
+and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+
+ПЕРЕВОД:
+В этом ката вы создадите функцию, которая принимает список неотрицательных целых чисел 
+и строк и возвращает новый список с отфильтрованными строками.
+
+Пример
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+*/
