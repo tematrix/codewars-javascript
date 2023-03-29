f2e9e9ae048d3475286210f1f10e38e8f4ddbcae
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-29 12:28:28 +0300

    You are square - description

---
+++ 7 kyo/you_are_a_square.js
@@ -0,0 +1,54 @@
+/*
+DESCRIPTION:
+A square of squares
+You like building blocks. You especially like building blocks that are squares. 
+And what you even like more, is to arrange them into a square of square building blocks!
+
+However, sometimes, you can't arrange them into a square. 
+Instead, you end up with an ordinary rectangle! Those blasted things! 
+If you just had a way to know, whether you're currently working in vain… Wait! 
+That's it! You just have to check if your number of building blocks is a perfect square.
+
+Task
+Given an integral number, determine if it's a square number:
+
+In mathematics, a square number or perfect square is an integer that is the square of an integer; 
+in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
+
+
+ПЕРЕВОД:
+Квадрат из квадратов
+Тебе нравятся строительные блоки. Вам особенно нравятся квадратные строительные блоки. 
+И что вам еще больше нравится, так это складывать их в квадрат из квадратных строительных блоков!
+
+Однако иногда вы не можете расположить их в квадрат. Вместо этого у вас получится обычный прямоугольник! 
+Эти проклятые твари! Если бы у вас только был способ узнать, не напрасно ли вы сейчас работаете… Подожди! 
+Вот и все! Вам просто нужно проверить, является ли ваше количество строительных блоков идеальным квадратом.
+
+Задача
+Учитывая целое число, определите, является ли это квадратным числом:
+
+В математике квадратное число или совершенный квадрат - это целое число, являющееся квадратом целого числа; 
+другими словами, это произведение некоторого целого числа на само себя.
+
+В тестах всегда будет использоваться некоторое целое число, 
+так что не беспокойтесь об этом в языках с динамической типизацией.
+
+Примеры
+-1 => ложь
+0 => верно
+3 => ложь
+4 => верно
+25 => верно
+26 => ложь
+*/
