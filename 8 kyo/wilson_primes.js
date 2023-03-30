3dbc3b57bbefaf0d84cfb7ec11fef1b36367ebcb
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-30 11:21:44 +0300

    Wilson primes - description

---
+++ 8 kyo/wilson_primes.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+Wilson primes satisfy the following condition. Let P represent a prime number.
+
+Then,
+
+((P-1)! + 1) / (P * P)
+should give a whole number.
+
+Your task is to create a function that returns true if the given number is a Wilson prime.
+
+
+ПЕРЕВОД:
+Простые числа Уилсона удовлетворяют следующему условию. Пусть P представляет собой простое число.
+
+Затем,
+
+((С-1)! + 1) / (P * P)
+должно давать целое число.
+
+Ваша задача - создать функцию, которая возвращает значение true, если данное число является простым числом Уилсона.
+*/
