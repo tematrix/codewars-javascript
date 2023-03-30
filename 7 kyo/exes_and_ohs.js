3e92966639a87cb58c094ca0f53c107db72fbc0b
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-30 11:16:35 +0300

    Exes and ohs - description

---
+++ 7 kyo/exes_and_ohs.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+Check to see if a string has the same amount of 'x's and 'o's. 
+The method must return a boolean and be case insensitive. 
+The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+
+
+ПЕРЕВОД:
+Проверьте, содержит ли строка одинаковое количество "x" и "o". 
+Метод должен возвращать логическое значение и быть нечувствительным к регистру. 
+Строка может содержать любой символ.
+
+Примеры ввода/вывода:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // при отсутствии 'x' и 'o' должно возвращаться значение true
+XO("zzoo") => false
+*/
