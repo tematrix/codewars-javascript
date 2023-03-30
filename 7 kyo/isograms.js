929baa068cfdcbaf773ef233a8f873ff6dc818f0
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-30 11:12:11 +0300

    Isograms - description

---
+++ 7 kyo/isograms.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+Implement a function that determines whether a string that contains only letters is an isogram. 
+Assume the empty string is an isogram. Ignore letter case.
+
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+isIsogram "Dermatoglyphics" = true
+isIsogram "moose" = false
+isIsogram "aba" = false
+
+
+ПЕРЕВОД:
+Изограмма - это слово, в котором нет повторяющихся букв, последовательных или непоследовательных 
+друг за другом. Реализуйте функцию, которая определяет, является ли строка, содержащая только 
+буквы, изограммой. Предположим, что пустая строка является изограммой. Игнорируйте регистр букв.
+
+Пример: (Ввод --> Вывод)
+
+"Дерматоглифы" -> истинное "aba" -> ложное "moOse" -> ложное (игнорировать регистр букв)
+
+Изограмма "Дерматоглифика" = истинная
+Изограмма "лось" = ложная
+Изограмма "аба" = ложная
+*/
