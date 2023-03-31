ea635ae355951d8dc32b5394910fcd4fd4b4ceb8
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-03-31 12:59:01 +0300

    Race ceremony - description

---
+++ 7 kyo/race_ceremony.js
@@ -0,0 +1,48 @@
+/*
+DESCRIPTION:
+The national go-kart racing competition is taking place at your local town and you've been 
+called for building the winners podium with the available wooden blocks. 
+Thankfully you are in a wood-rich area, number of blocks are always at least 6.
+
+Remember a classic racing podium has three platforms for first, second and third place. 
+First place is the highest and second place is higher than third. 
+Also notice that platforms are arranged as 2nd - 1st - 3rd.
+
+The organizers want a podium that satisfies:
+
+The first place platform has the minimum height possible
+The second place platform has the closest height to first place
+All platforms have heights greater than zero.
+Task
+Given the numbers of blocks available, return an array / tuple or another data structure 
+depending on the language (refer sample tests) with the heights of 2nd, 1st, 3rd places platforms.
+
+Examples (input -> output)
+11 ->   [4, 5, 2]
+6  ->   [2, 3, 1]
+10 ->   [4, 5, 1]
+
+
+ПЕРЕВОД:
+В вашем городе проходят национальные соревнования по картингу, и вас пригласили построить 
+пьедестал почета победителей из имеющихся деревянных блоков. К счастью, вы находитесь в районе, 
+богатом лесом, количество блоков всегда не менее 6.
+
+Помните, что на классическом гоночном подиуме есть три платформы для первого, второго и третьего мест. 
+Первое место - самое высокое, а второе место выше третьего. 
+Также обратите внимание, что платформы расположены следующим образом: 2-я - 1-я - 3-я.
+
+Организаторам нужен подиум, который удовлетворял бы:
+
+Платформа для первого места имеет минимально возможную высоту
+Платформа для второго места имеет самую близкую высоту к первому месту
+Высота всех платформ больше нуля.
+Задача
+Учитывая количество доступных блоков, верните массив / кортеж или другую структуру данных 
+в зависимости от языка (см. примеры тестов) с высотой платформ 2-го, 1-го, 3-го мест.
+
+Примеры (ввод -> вывод)
+11 -> [4, 5, 2]
+6 -> [2, 3, 1]
+10 -> [4, 5, 1]
+*/
