0d5f2069ca8c7dd467c42e36d054fd18f8c1faec
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-01 13:53:19 +0300

    Shortest word - description

---
+++ 7 kyo/shortest_word.js
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+
+ПЕРЕВОД:
+Просто, учитывая строку слов, верните длину самого короткого слова(ов).
+
+Строка никогда не будет пустой, и вам не нужно учитывать разные типы данных.
+*/
