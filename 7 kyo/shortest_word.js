6720503480104c8045c335ef4a3ee976e31eb1cb
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-01 13:55:37 +0300

    Shortest word - decision

--- 7 kyo/shortest_word.js
+++ 7 kyo/shortest_word.js
@@ -10,3 +10,17 @@ String will never be empty and you do not need to account for different data typ
 
 Строка никогда не будет пустой, и вам не нужно учитывать разные типы данных.
 */
+
+function findShort(s){
+    const a = s.split(' ');
+    let min = Infinity;
+    a.forEach(e => {
+      if (e.length < min) {
+        min = e.length;
+      }
+    }); 
+    return min;
+}
+
+console.log(findShort('Simple, given a string of words, return the length of the shortest word'));
+console.log(findShort('String will never be empty and you do not need to account for different data types'));
