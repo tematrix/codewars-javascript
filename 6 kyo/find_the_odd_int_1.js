bf5454afad3bec4bcd2b41dd72910c6842e81a4e
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-02 17:44:08 +0300

    Find the odd int (ver1) - description

---
+++ 6 kyo/find_the_odd_int_1.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+
+ПЕРЕВОД:
+Учитывая массив целых чисел, найдите то, которое встречается нечетное число раз.
+
+Всегда будет только одно целое число, которое появляется нечетное число раз.
+
+Примеры
+[7] должно возвращать 7, потому что это происходит 1 раз (что нечетно).
+[0] должно возвращать 0, потому что это происходит 1 раз (что нечетно).
+[1,1,2] должно возвращать 2, потому что это происходит 1 раз (что нечетно).
+[0,1,0,1,0] должно возвращать 0, потому что это происходит 3 раза (что нечетно).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] должно возвращать 4, потому что оно появляется 1 раз (что нечетно).
+*/
