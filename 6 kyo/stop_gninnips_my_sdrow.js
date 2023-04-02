0aded4573c378c4b7516f7a5b5ea2d65652ccf66
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-02 17:38:31 +0300

    Stop gninnips my sdrow - description

---
+++ 6 kyo/stop_gninnips_my_sdrow.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+Write a function that takes in a string of one or more words, and returns the same string, 
+but with all five or more letter words reversed (Just like the name of this Kata). 
+Strings passed in will consist of only letters and spaces. 
+Spaces will be included only when more than one word is present.
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+
+
+ПЕРЕВОД:
+Напишите функцию, которая принимает строку из одного или нескольких слов и возвращает ту же строку, 
+но со всеми пятью или более буквенными словами наоборот (точно так же, как название этого ката). 
+Передаваемые строки будут состоять только из букв и пробелов. 
+Пробелы будут включены только в том случае, если присутствует более одного слова.
+
+Примеры:
+
+spin Words( "Эй, товарищи воины" ) => возвращает "Эй, воллеф сроиррау"
+spinWords("Это тест") => возвращает "Это тест"
+spinWords("Это еще один тест" )=> возвращает "Это тест рехтона"
+*/
