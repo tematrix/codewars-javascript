ffb9b713d3c813d920aea00fcb435c3f55d64fc0
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-03 10:45:19 +0300

    Sum of digits Digital root (ver1) - description

---
+++ 6 kyo/sum_of_digits_digital_root_1.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, 
+continue reducing in this way until a single-digit number is produced. 
+The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+
+ПЕРЕВОД:
+Цифровой корень - это рекурсивная сумма всех цифр в числе.
+
+Учитывая n, возьмите сумму цифр n. Если это значение содержит более одной цифры, 
+продолжайте уменьшать таким образом до тех пор, пока не будет получено однозначное число. 
+Входным значением будет неотрицательное целое число.
+
+Примеры
+16 --> 1 + 6 = 7
+942 --> 9 + 4 + 2 = 15 --> 1 + 5 = 6
+132189 --> 1 + 3 + 2 + 1 + 8 + 9 = 24 --> 2 + 4 = 6
+493193 --> 4 + 9 + 3 + 1 + 9 + 3 = 29 --> 2 + 9 = 11 --> 1 + 1 = 2
+*/
