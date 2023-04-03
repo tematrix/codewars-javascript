d66cfb9bdd88b477a7a9f836dd4e50959bc37531
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-03 10:57:27 +0300

    Array diff - description

---
+++ 6 kyo/array_diff.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+Your goal in this kata is to implement a difference function, 
+which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+
+ПЕРЕВОД:
+Ваша цель в этом ката - реализовать разностную функцию, 
+которая вычитает один список из другого и возвращает результат.
+
+Он должен удалить все значения из списка a, которые присутствуют в списке b, сохраняя их порядок.
+
+разница в массиве([1,2],[1]) == [2]
+Если значение присутствует в b, все его вхождения должны быть удалены из другого:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+*/
