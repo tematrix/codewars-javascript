aa0ccaa402954484054ff798724eef69ab2f724a
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-04 11:06:38 +0300

    Create phone number - description

---
+++ 6 kyo/create_phone_number.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+Write a function that accepts an array of 10 integers (between 0 and 9), 
+that returns a string of those numbers in the form of a phone number.
+
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+
+Don't forget the space after the closing parentheses!
+
+
+ПЕРЕВОД:
+Напишите функцию, которая принимает массив из 10 целых чисел (от 0 до 9), 
+который возвращает строку из этих чисел в виде телефонного номера.
+
+Пример
+создания телефонного номера([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => возвращает "(123) 456-7890"
+Возвращаемый формат должен быть правильным, чтобы выполнить эту задачу.
+
+Не забудьте про пробел после закрывающих скобок!
+*/
