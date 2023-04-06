bc1646447660cbf77aff66163c21f8de3aefca19
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-06 10:56:45 +0300

    Replace with alphabet position - decision

--- 6 kyo/replace_with_alphabet_position.js
+++ 6 kyo/replace_with_alphabet_position.js
@@ -26,3 +26,18 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 расположения в алфавите ("Закат заходит в двенадцать часов").
 Должен вернуться "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( в виде строки )
 */
+
+function alphabetPosition(text) {
+    const alpha = ['a','b','c','d','e','f','g','h','i','j','k','l',
+          'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const result = [];
+    text = text.toLowerCase().split('');
+    for (let i of text) {
+      if (alpha.find(e => e == i)) {
+        result.push(alpha.findIndex(e => e == i) + 1);      
+      }
+    }
+    return result.join(' ');
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."));
