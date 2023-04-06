3d6f4360ed7aa4b9f43a81ca67502fbd326bb929
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-06 10:54:07 +0300

    Replace with alphabet position - description

---
+++ 6 kyo/replace_with_alphabet_position.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+
+ПЕРЕВОД:
+Добро пожаловать.
+
+В этом ката от вас требуется, получив строку, заменить каждую букву на ее позицию в алфавите.
+
+Если что-то в тексте не является буквой, проигнорируйте это и не возвращайте.
+
+"a" = 1, "b" = 2 и т.д.
+
+Пример
+расположения в алфавите ("Закат заходит в двенадцать часов").
+Должен вернуться "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( в виде строки )
+*/
