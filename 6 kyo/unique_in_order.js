d107f900644d41b226e9a4e153201f614a173e94
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-07 10:48:54 +0300

    Unique in order - description

---
+++ 6 kyo/unique_in_order.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+Implement the function unique_in_order which takes as argument a sequence and returns 
+a list of items without any elements with the same value next to each other 
+and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+
+ПЕРЕВОД:
+Реализуйте функцию unique_in_order, которая принимает в качестве аргумента последовательность 
+и возвращает список элементов без каких-либо элементов с одинаковым значением 
+рядом друг с другом и сохранением исходного порядка элементов.
+
+Например:
+
+уникальный по порядку('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+Уникальный по порядку('ABBCcAD') == ['A', 'B', 'C', 'c', 'A', 'D']
+Уникальный порядок([1,2,2,3,3]) == [1,2,3]
+*/
