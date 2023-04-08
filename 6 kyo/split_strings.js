ca470b408a834473ad7646388f09dfb688453ea1
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-08 10:11:38 +0300

    Split strings - description

---
+++ 6 kyo/split_strings.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+Complete the solution so that it splits the string into pairs of two characters. 
+If the string contains an odd number of characters then it should replace 
+the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+
+
+ПЕРЕВОД:
+Доработайте решение так, чтобы оно разбивало строку на пары из двух символов. 
+Если строка содержит нечетное количество символов, то замените недостающий 
+второй символ последней пары знаком подчеркивания ('_').
+
+Примеры:
+
+* 'abc' => ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef'].
+*/
