1266bf05a2718894e5f72a582a48eeee80af5b30
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-08 09:55:12 +0300

    Sort the odd - description

---
+++ 6 kyo/sort_the_odd.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+You will be given an array of numbers. 
+You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+
+ПЕРЕВОД:
+Вам будет дан массив чисел. 
+Вы должны отсортировать нечетные числа по возрастанию, а четные оставить на прежних местах.
+
+Примеры
+[7, 1] => [1, 7]
+[5, 8, 6, 3, 4] => [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0] => [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+*/
