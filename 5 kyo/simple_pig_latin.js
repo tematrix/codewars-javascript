130dce5ccff7ec25ca226904ac1f572890fc9d09
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-09 12:02:53 +0300

    Simple pig latin - description

---
+++ 5 kyo/simple_pig_latin.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
+Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+
+
+ПЕРЕВОД:
+Перенесите первую букву каждого слова в его конец, затем добавьте "ay" в конец слова. 
+Знаки препинания оставьте нетронутыми.
+
+Примеры
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world!'); // elloHay orldway!
+*/
