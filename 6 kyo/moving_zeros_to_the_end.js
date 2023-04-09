ecc4381e6ef1e09d1fb388fde5a24834b6cef882
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-09 11:58:12 +0300

    Moving zeros to the end - description

---
+++ 6 kyo/moving_zeros_to_the_end.js
@@ -0,0 +1,14 @@
+/*
+DESCRIPTION:
+Write an algorithm that takes an array and moves all of the zeros to the end, 
+preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+
+ПЕРЕВОД:
+Напишите алгоритм, который берет массив и перемещает все нули в конец, 
+сохраняя порядок остальных элементов.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
