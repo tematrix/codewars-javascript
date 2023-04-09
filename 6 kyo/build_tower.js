2129a5d0c548eafa8965579d76f85f104eddeb8e
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-09 11:54:20 +0300

    Build tower - description

---
+++ 6 kyo/build_tower.js
@@ -0,0 +1,46 @@
+/*
+DESCRIPTION:
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. 
+A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+]
+
+
+ПЕРЕВОД:
+Постройте башню в форме пирамиды в виде массива/списка строк, заданного целым положительным числом этажей. 
+Блок башни представлен символом "*".
+
+Например, башня с 3 этажами выглядит следующим образом:
+
+[
+  "  * ",
+  " *** ", 
+  "*****"
+]
+А башня с 6 этажами выглядит следующим образом:
+
+[
+  "     * ", 
+  "    *** ", 
+  "   ***** ", 
+  "  ******* ", 
+  " ********* ", 
+  "***********"
+]
+*/
