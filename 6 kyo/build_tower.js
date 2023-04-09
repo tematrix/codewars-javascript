8f43bdb190931fc8dbb17b09f8dde50812a84d22
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-09 11:56:28 +0300

    Build tower - solution

--- 6 kyo/build_tower.js
+++ 6 kyo/build_tower.js
@@ -44,3 +44,23 @@ And a tower with 6 floors looks like this:
   "***********"
 ]
 */
+
+function towerBuilder(n) {
+    const sps = (c) => {
+      return c <= 1 ? `` : ` ` + sps(c-1);    
+    };
+    const sts = (c) => {
+      return c <= 1 ? `*` : `**` + sts(c-1);
+    };
+    
+    let r = [];
+    
+    for (let i = 0; i < n; i++) {
+      r[i] = `${sps(n-i)}${sts(i+1)}${sps(n-i)}`;
+    }
+    
+    return r;  
+}
+
+console.log(towerBuilder(3));
+console.log(towerBuilder(6));
