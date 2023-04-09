7f00cacef04a8e074447d2b081d688ac70d3acaa
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-09 12:04:44 +0300

    Simple pig latin - solution

--- 5 kyo/simple_pig_latin.js
+++ 5 kyo/simple_pig_latin.js
@@ -16,3 +16,21 @@ pigIt('Hello world !');     // elloHay orldway !
 pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 pigIt('Hello world!'); // elloHay orldway!
 */
+
+function pigIt(str){
+    let arr = str.split(' '), res = [];
+    for (let e of arr) {
+      if (e !== '!' && e !== '.' && e !== ',' && e !== '?') {
+        let s = e.split('');
+        s.push(`${s[0]}ay`);
+        s.shift();
+        res.push(s.join(''));
+      } else {
+        res.push(e);
+      }   
+    }
+    return res.join(' ');
+}
+
+console.log(pigIt('Pig latin is cool'));
+console.log(pigIt('Hello world !'));
