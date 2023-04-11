5074bfcf801be743255591fd90cbcbbb15c9ee78
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-11 10:59:18 +0300

    RGB to hex conversion (ver2) - description

---
+++ 5 kyo/rgb_to_hex_conversion_2.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION:
+The rgb function is incomplete. Complete it so that passing in RGB decimal values 
+will result in a hexadecimal representation being returned. 
+Valid decimal values for RGB are 0 - 255. 
+Any values that fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+The following are examples of expected output values:
+
+rgb(255, 255, 255) // returns FFFFFF
+rgb(255, 255, 300) // returns FFFFFF
+rgb(0,0,0) // returns 000000
+rgb(148, 0, 211) // returns 9400D3
+
+
+ПЕРЕВОД:
+Функция rgb является неполной. Доработайте ее так, чтобы при передаче десятичных значений RGB 
+приводила к возврату шестнадцатеричного представления. 
+Допустимые десятичные значения для RGB - 0 - 255. 
+Любые значения, выходящие за пределы этого диапазона, должны быть округлены до ближайшего допустимого значения.
+
+Примечание: Ваш ответ всегда должен быть длиной 6 символов, сокращение с 3 здесь не подходит.
+
+Ниже приведены примеры ожидаемых выходных значений:
+
+rgb(255, 255, 255) // возвращает FFFFFF
+rgb(255, 255, 300) // возвращает FFFFFF
+rgb(0,0,0) // возвращает 000000
+rgb(148, 0, 211) // возвращает 9400D3
+*/
