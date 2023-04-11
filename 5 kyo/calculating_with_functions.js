05f7d567d6b5f4bc17182e6c18dec5c87b735572
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-11 11:07:52 +0300

    Calculating with functions - solution

--- 5 kyo/calculating_with_functions.js
+++ 5 kyo/calculating_with_functions.js
@@ -34,3 +34,54 @@ six(dividedBy(two())); // должно возвращать 3
 Деление должно быть целочисленным. Например, это должно вернуть 2, а не 2.666666...:
 eight(dividedBy(three()));
 */
+
+'use strict';
+
+function zero() {
+    return zero.arguments.length > 0 ? Math.floor(eval(`0${arguments[0]}`)) : 0;
+  }
+  function one() {
+    return one.arguments.length > 0 ? Math.floor(eval(`1${arguments[0]}`)) : 1;
+  }
+  function two() {
+    return two.arguments.length > 0 ? Math.floor(eval(`2${arguments[0]}`)) : 2;
+  }
+  function three() {
+    return three.arguments.length > 0 ? Math.floor(eval(`3${arguments[0]}`)) : 3;
+  }
+  function four() {
+    return four.arguments.length > 0 ? Math.floor(eval(`4${arguments[0]}`)) : 4;
+  }
+  function five() {
+    return five.arguments.length > 0 ? Math.floor(eval(`5${arguments[0]}`)) : 5;
+  }
+  function six() {
+    return six.arguments.length > 0 ? Math.floor(eval(`6${arguments[0]}`)) : 6;
+  }
+  function seven() {
+    return seven.arguments.length > 0 ? Math.floor(eval(`7${arguments[0]}`)) : 7;
+  }
+  function eight() {
+    return eight.arguments.length > 0 ? Math.floor(eval(`8${arguments[0]}`)) : 8;
+  }
+  function nine() {
+    return nine.arguments.length > 0 ? Math.floor(eval(`9${arguments[0]}`)) : 9;
+  }
+  
+  function plus() {
+    return `+${arguments[0]}`;
+  }
+  function minus() {
+    return `-${arguments[0]}`;
+  }
+  function times() {
+    return `*${arguments[0]}`;
+  }
+  function dividedBy() {
+    return `/${arguments[0]}`;
+  }
+
+console.log(seven(times(five())));
+console.log(four(plus(nine())));
+console.log(eight(minus(three())));
+console.log(six(dividedBy(two())));
