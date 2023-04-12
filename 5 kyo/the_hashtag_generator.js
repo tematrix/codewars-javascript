fbd15b5f6be48d763e31e1a8e78ef0bcec6ef631
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-12 13:36:41 +0300

    The hashtag generator - description

---
+++ 5 kyo/the_hashtag_generator.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION:
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+Examples
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+
+
+ПЕРЕВОД:
+Маркетологи тратят слишком много времени на введение хэштегов.
+Давайте поможем им с помощью нашего собственного генератора хэштегов!
+
+Вот что нужно сделать:
+
+Все слова должны начинаться с хэштега (#).
+Все слова должны быть написаны с заглавной буквы.
+Если конечный результат длиннее 140 символов, он должен вернуть false.
+Если входные данные или результат - пустая строка, то он должен возвращать false.
+Примеры
+" Здравствуйте, спасибо, что попробовали мое ката" => "#HelloThereThanksForTryingMyKata"
+"    Hello World" => "#HelloWorld"
+"" => false
+*/
