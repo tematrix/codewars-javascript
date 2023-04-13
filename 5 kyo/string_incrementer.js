d5263801e93e41d930d67135ac72bfd78ff0d937
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-13 10:54:38 +0300

    String incrementer - description

---
+++ 5 kyo/string_incrementer.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION:
+Your job is to write a function which increments a string, to create a new string.
+
+If the string already ends with a number, the number should be incremented by 1.
+If the string does not end with a number. the number 1 should be appended to the new string.
+Examples:
+
+foo -> foo1
+foobar23 -> foobar24
+foo0042 -> foo0043
+foo9 -> foo10
+foo099 -> foo100
+
+Attention: If the number has leading zeros the amount of digits should be considered.
+
+
+ПЕРЕВОД:
+Ваша задача - написать функцию, которая увеличивает строку, чтобы создать новую строку.
+
+Если строка уже заканчивается числом, то это число должно быть увеличено на 1.
+Если строка не заканчивается числом, то к новой строке должно быть добавлено число 1.
+Примеры:
+
+foo -> foo1
+foobar23 -> foobar24
+foo0042 -> foo0043
+foo9 -> foo10
+foo099 -> foo100
+
+Внимание: Если в числе есть ведущие нули, необходимо учитывать количество цифр.
+*/
