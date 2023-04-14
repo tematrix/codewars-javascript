fc5eb3d1efc0f9fdb532c01056cb9e1352ff939f
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-14 10:22:49 +0300

    Extract the domain name - description

---
+++ 5 kyo/extract_the_domain_name.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Write a function that when given a URL as a string, parses out just the domain name 
+and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+
+
+ПЕРЕВОД:
+Напишите функцию, которая, когда ей передается URL в виде строки, выбирает 
+только имя домена и возвращает его в виде строки. Например:
+
+url = "http://github.com/carbonfive/raygun" -> имя домена = "github"
+url = "http://www.zombie-bites.com" -> имя домена = "zombie-bites"
+url = "https://www.cnet.com" -> имя домена = "cnet"
+*/
