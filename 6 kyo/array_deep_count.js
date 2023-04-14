763c383ab2f6f1799a5d447e5dc0decd87f4bc52
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-14 10:17:22 +0300

    Array deep count - description

---
+++ 6 kyo/array_deep_count.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+You are given an array. Complete the function that returns the number of 
+ALL elements within an array, including any nested arrays.
+
+Examples
+[]                   -->  0
+[1, 2, 3]            -->  3
+["x", "y", ["z"]]    -->  4
+[1, 2, [3, 4, [5]]]  -->  7
+
+The input will always be an array.
+
+
+ПЕРЕВОД:
+Дан массив. Необходимо завершить функцию, которая возвращает количество 
+всех элементов в массиве, включая любые вложенные массивы.
+
+Примеры:
+[] --> 0
+[1, 2, 3] --> 3
+["x", "y", ["z"]] --> 4
+[1, 2, [3, 4, [5]]] --> 7
+
+Входной параметр всегда будет массивом.
+*/
