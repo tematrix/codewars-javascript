2f7d8b257ec287e292e0096597258b0cf4d55eb3
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-14 10:11:46 +0300

    Detect pangram - description

---
+++ 6 kyo/detect_pangram.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+A pangram is a sentence that contains every single letter of the alphabet at least once. 
+For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, 
+because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. 
+Return True if it is, False if not. Ignore numbers and punctuation.
+
+
+ПЕРЕВОД:
+Панграмма - это предложение, которое содержит каждую букву алфавита хотя бы один раз. 
+Например, предложение "The quick brown fox jumps over the lazy dog" является панграммой, 
+потому что оно использует буквы A-Z хотя бы один раз (регистр не имеет значения).
+
+Дана строка. Необходимо определить, является ли она панграммой. 
+Если да, вернуть True, если нет - False. Цифры и знаки препинания не учитывать.
+*/
