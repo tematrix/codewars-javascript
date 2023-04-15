8d1d59df870147fa292dc02274628cc7d591982d
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-15 09:54:11 +0300

    Regex password validation - description

---
+++ 5 kyo/regex_password_validation.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+You need to write regex that will validate a password to make sure it meets the following criteria:
+
+- At least six characters long
+- contains a lowercase letter
+- contains an uppercase letter
+- contains a digit
+- only contains alphanumeric characters (note that '_' is not alphanumeric)
+
+
+ПЕРЕВОД:
+Вам нужно написать регулярное выражение, которое будет проверять пароль на соответствие следующим критериям:
+
+- Длина пароля должна быть не менее шести символов.
+- Пароль должен содержать строчную букву.
+- Пароль должен содержать заглавную букву.
+- Пароль должен содержать цифру.
+- Пароль может содержать только буквенно-цифровые символы (заметьте, что символ '_' не является буквенно-цифровым).
+*/
