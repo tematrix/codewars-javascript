64c19ac651edc1ab48468950210f3db01777f6ea
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-15 09:41:54 +0300

    Scramblies - description

---
+++ 5 kyo/scramblies.js
@@ -0,0 +1,29 @@
+/*
+DESCRIPTION:
+Complete the function scramble(str1, str2) that returns true if a portion of str1 characters 
+can be rearranged to match str2, otherwise returns false.
+
+Notes:
+
+Only lower case letters will be used (a-z). No punctuation or digits will be included.
+Performance needs to be considered.
+Examples
+scramble('rkqodlw', 'world') ==> True
+scramble('cedewaraaossoqqyt', 'codewars') ==> True
+scramble('katas', 'steak') ==> False
+
+
+ПЕРЕВОД:
+Реализуйте функцию scramble(str1, str2), которая возвращает true, если часть символов из str1 
+может быть переставлена так, чтобы получить str2, в противном случае возвращает false.
+
+Примечания:
+
+Входные строки содержат только символы в нижнем регистре (a-z). Никаких знаков препинания или цифр не будет.
+Необходимо учитывать производительность.
+
+Примеры:
+scramble('rkqodlw', 'world') ==> True
+scramble('cedewaraaossoqqyt', 'codewars') ==> True
+scramble('katas', 'steak') ==> False
+*/
