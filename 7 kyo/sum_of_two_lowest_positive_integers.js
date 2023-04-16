cf03a816186d97e4d395630db84286f670a95647
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-16 10:42:40 +0300

    Sum of two lowest positive integers - solution

--- 7 kyo/sum_of_two_lowest_positive_integers.js
+++ 7 kyo/sum_of_two_lowest_positive_integers.js
@@ -16,3 +16,11 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 
 [10, 343445353, 3453445, 3453545353453] должен вернуть 3453455.
 */
+
+function sumTwoSmallestNumbers(numbers) {  
+    numbers = numbers.sort((a,b) => a-b);
+    return numbers[0]+numbers[1];
+}
+
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
+console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]));
