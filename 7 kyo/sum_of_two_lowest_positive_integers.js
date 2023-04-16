efdb78e0ff565eeb7f0e02d5f0a075be6160a3e9
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-16 10:40:53 +0300

    Sum of two lowest positive integers - description

---
+++ 7 kyo/sum_of_two_lowest_positive_integers.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Create a function that returns the sum of the two lowest positive numbers given an array 
+of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+
+ПЕРЕВОД:
+Напишите функцию, которая возвращает сумму двух наименьших положительных чисел из массива 
+минимум из 4 положительных целых чисел. В массиве не будет чисел с плавающей запятой или отрицательных чисел.
+
+Например, при передаче массива [19, 5, 42, 2, 77] функция должна вернуть 7.
+
+[10, 343445353, 3453445, 3453545353453] должен вернуть 3453455.
+*/
