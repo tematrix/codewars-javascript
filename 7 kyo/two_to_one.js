f0af59431d8f708c1e1cbd7ddabbf19f13d907bb
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-16 10:36:39 +0300

    Two to one - description

---
+++ 7 kyo/two_to_one.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+Take 2 strings s1 and s2 including only letters from a to z. 
+Return a new sorted string, the longest possible, containing distinct letters - 
+each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+
+ПЕРЕВОД:
+Даны две строки s1 и s2, содержащие только буквы от a до z. 
+Необходимо вернуть новую отсортированную строку, максимально длинную, содержащую уникальные буквы - 
+каждая буква должна встречаться только один раз - из s1 или s2.
+
+Примеры:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+*/
