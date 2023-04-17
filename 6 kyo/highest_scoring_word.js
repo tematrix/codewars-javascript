e2db7c0ef3b6825912657744b2381243d442dea9
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-17 10:54:08 +0300

    Highest scoring word - description

---
+++ 6 kyo/highest_scoring_word.js
@@ -0,0 +1,29 @@
+/*
+DESCRIPTION:
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+For example, the score of abad is 8 (1 + 2 + 1 + 4).
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+
+
+ПЕРЕВОД:
+Дана строка слов, вам нужно найти слово с наибольшим баллом.
+
+Каждая буква слова оценивается определенным количеством очков в соответствии 
+с ее позицией в алфавите: a = 1, b = 2, c = 3 и так далее.
+
+Например, балл слова abad равен 8 (1 + 2 + 1 + 4).
+
+Вам нужно вернуть слово с наибольшим баллом в виде строки.
+
+Если два слова получают одинаковый балл, верните слово, которое появляется раньше в исходной строке.
+
+Все буквы будут в нижнем регистре, а все входные данные будут допустимыми.
+*/
