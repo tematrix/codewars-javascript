1194bad269f81c2aded88eb1503e742d120821ad
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-19 11:38:05 +0300

    Write number in expanded form - description (ver1)

---
+++ 6 kyo/write_number_in_expanded_form_1.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+
+NOTE: All numbers will be whole numbers greater than 0.
+
+
+ПЕРЕВОД:
+Вы получите число, и вам нужно вернуть его в виде строки в расширенной форме. Например:
+
+expandedForm(12); // Должно вернуть '10 + 2'
+expandedForm(42); // Должно вернуть '40 + 2'
+expandedForm(70304); // Должно вернуть '70000 + 300 + 4'
+
+Примечание: Все числа будут целыми числами, большими 0.
+*/
