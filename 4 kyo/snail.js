c1f3c436962d669d249b5c52ef3c42e0b41e024a
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-21 10:44:24 +0300

    Snail - description

---
+++ 4 kyo/snail.js
@@ -0,0 +1,45 @@
+/*
+DESCRIPTION:
+Given an n x n array, return the array elements arranged from outermost elements 
+to the middle element, traveling clockwise.
+
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+snail(array) #=> [1,2,3,6,9,8,7,4,5]
+For better understanding, please follow the numbers of the next array consecutively:
+
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+snail(array) #=> [1,2,3,4,5,6,7,8,9]
+
+NOTE: The idea is not sort the elements from the lowest value to the highest; 
+the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+
+NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
+
+
+ПЕРЕВОД:
+Дан массив размером n x n. Вернуть элементы массива, расположенные от внешних элементов 
+к среднему элементу, двигаясь по часовой стрелке.
+
+Массив:
+
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+Функция snail(array) должна вернуть [1,2,3,6,9,8,7,4,5].
+
+Для лучшего понимания следуйте номерам в следующем массиве последовательно:
+
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+Функция snail(array) должна вернуть [1,2,3,4,5,6,7,8,9].
+
+Примечание: идея не в сортировке элементов от наименьшего значения к наибольшему; 
+идея заключается в обходе двумерного массива в круговом порядке, двигаясь по часовой стрелке.
+
+Примечание 2: пустая матрица 0x0 (empty matrix) представлена пустым массивом внутри массива [[]].
+*/
