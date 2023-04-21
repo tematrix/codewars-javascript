853f9690439ab9c363b39490f63864bc65ceb22e
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-21 10:49:22 +0300

    Does my number look big - description

---
+++ 6 kyo/does_my_number_look_big.js
@@ -0,0 +1,47 @@
+/*
+DESCRIPTION:
+A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its 
+own digits, each raised to the power of the number of digits in a given base. 
+In this Kata, we will restrict ourselves to decimal (base 10).
+
+For example, take 153 (3 digits), which is narcissistic:
+
+    1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+and 1652 (4 digits), which isn't:
+
+    1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+
+The Challenge:
+
+Your code must return true or false (not 'true' and 'false') depending upon whether 
+the given number is a Narcissistic number in base 10.
+
+This may be True and False in your language, e.g. PHP.
+
+Error checking for text strings or other invalid inputs is not required, 
+only valid positive non-zero integers will be passed into the function.
+
+
+ПЕРЕВОД:
+Число Нарциста (или число Армстронга) - это положительное число, которое является 
+суммой его собственных цифр, возведенных в степень количества цифр в данной системе счисления. 
+В этом ката мы ограничимся десятичной системой счисления (основание 10).
+
+Например, возьмем число 153 (3 цифры), которое является числом Нарциста:
+
+1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
+А число 1652 (4 цифры) Нарцистическим не является:
+
+1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+
+Задача:
+
+Ваш код должен возвращать true или false (а не 'true' и 'false') в зависимости от того, 
+является ли данное число Нарцистическим числом в десятичной системе счисления.
+
+В вашем языке программирования это может быть True и False, например, в PHP.
+
+Проверка ошибок для текстовых строк или других недопустимых входных данных не требуется, 
+только допустимые положительные ненулевые целые числа будут переданы в функцию.
+*/
