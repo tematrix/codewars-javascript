d8b1a1d9b53f92ad1a222baac9fa6c84b964b54f
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-22 10:12:14 +0300

    Next bigger number with the same digits - description

---
+++ 4 kyo/next_bigger_number_with_the_same_digits.js
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION:
+Create a function that takes a positive integer and returns the next bigger number 
+that can be formed by rearranging its digits. For example:
+
+  12 ==> 21
+ 513 ==> 531
+2017 ==> 2071
+If the digits can't be rearranged to form a bigger number, return -1 (or nil in Swift, None in Rust):
+
+  9 ==> -1
+111 ==> -1
+531 ==> -1
+
+
+ПЕРЕВОД:
+Создайте функцию, которая принимает положительное целое число и возвращает следующее большее число, 
+которое может быть получено перестановкой его цифр. Например:
+
+12 ==> 21
+513 ==> 531
+2017 ==> 2071
+
+Если цифры не могут быть переставлены так, чтобы получить большее число, 
+верните -1 (или nil в Swift, None в Rust):
+
+9 ==> -1
+111 ==> -1
+531 ==> -1
+*/
