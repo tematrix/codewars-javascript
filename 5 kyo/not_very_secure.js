eec12b92a8ae5f4d187f3d9266749d3ac67dc48d
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-24 01:44:48 +0300

    Not very secure - description

---
+++ 5 kyo/not_very_secure.js
@@ -0,0 +1,23 @@
+/*
+DESCRIPTION:
+In this example you have to validate if a user input string is alphanumeric. 
+The given string is not nil/null/NULL/None, so you don't have to check that.
+
+The string has the following conditions to be alphanumeric:
+
+At least one character ("" is not valid)
+Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+No whitespaces / underscore
+
+
+ПЕРЕВОД:
+В этом примере вам нужно проверить, является ли пользовательский ввод строкой, 
+содержащей буквы и цифры (алфанумерической). Данная строка не является nil/null/NULL/None, 
+поэтому эту проверку выполнять не нужно.
+
+Строка должна соответствовать следующим условиям, чтобы быть алфанумерической:
+
+Содержать хотя бы один символ ("" не является допустимым)
+Допустимы заглавные и строчные буквы латинского алфавита и цифры от 0 до 9
+Не допускаются пробелы и символ подчеркивания
+*/
