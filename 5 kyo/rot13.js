7d8a39c36cb74f7ff98ebf43ab5bebfe690ac437
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-24 01:40:36 +0300

    Rot13 - description

---
+++ 5 kyo/rot13.js
@@ -0,0 +1,19 @@
+/*
+DESCRIPTION:
+ROT13 is a simple letter substitution cipher that replaces a letter with the 
+letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13. 
+If there are numbers or special characters included in the string, they should 
+be returned as they are. Only letters from the latin/english alphabet should be shifted, 
+like in the original Rot13 "implementation".
+
+
+ПЕРЕВОД:
+ROT13 - это простой шифр подстановки букв, который заменяет букву на букву, 
+идущую на 13 позиций после неё в алфавите. ROT13 является примером шифра Цезаря.
+
+Создайте функцию, которая принимает строку и возвращает зашифрованную с помощью ROT13 строку. 
+Если в строке присутствуют цифры или специальные символы, они должны быть возвращены без изменений. 
+Только буквы латинского/английского алфавита должны быть сдвинуты, как в оригинальной реализации ROT13.
+*/
