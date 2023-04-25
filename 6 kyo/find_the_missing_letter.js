a43f686f572520822738b9a1964426cb0acb76ea
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-26 01:25:34 +0300

    Find the missing letter - description

---
+++ 6 kyo/find_the_missing_letter.js
@@ -0,0 +1,29 @@
+/*
+DESCRIPTION:
+Write a method that takes an array of consecutive (increasing) 
+letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. 
+The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+(Use the English alphabet with 26 letters!)
+
+
+ПЕРЕВОД:
+Напишите метод, который принимает на вход массив последовательных (возрастающих) 
+букв и возвращает отсутствующую букву в массиве.
+
+Вы всегда получите допустимый массив. И в нем всегда будет ровно одна отсутствующая буква. 
+Длина массива всегда будет не менее 2. В массиве всегда будут буквы только в одном регистре.
+
+Примеры:
+
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+(Используйте английский алфавит с 26 буквами!)
+*/
