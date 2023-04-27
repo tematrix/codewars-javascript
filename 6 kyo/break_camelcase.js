b5b66b5e62c4499d828e82c878d475e602d90a78
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-27 10:40:09 +0300

    Break camelcase - description

---
+++ 6 kyo/break_camelcase.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+
+ПЕРЕВОД:
+Задача функции - разделить строку в стиле "camel case" на отдельные слова, вставив между ними пробелы.
+
+Пример:
+"camelCasing" => "camel Casing"
+"identifier" => "identifier"
+"" => ""
+
+Функция должна принимать строку в стиле "camel case" в качестве аргумента и возвращать новую строку, 
+в которой каждое новое слово начинается с заглавной буквы, а все слова разделены пробелами. 
+Если исходная строка пуста, функция должна вернуть пустую строку.
+*/
