39670db2f39013cefa73520278b02b3491b74124
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-28 14:04:12 +0300

    Count characters in your string - description

---
+++ 6 kyo/count_characters_in_your_string.js
@@ -0,0 +1,13 @@
+/*
+DESCRIPTION:
+The main idea is to count all the occurring characters in a string. 
+If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+
+ПЕРЕВОД:
+Основная идея заключается в подсчете всех встречающихся символов в строке.
+Если у вас есть строка, например, "aba", то результат должен быть {'a': 2, 'b': 1}.
+
+Что, если строка пуста? Тогда результатом должен быть пустой объект, {} (пустой литерал объекта).
+*/
