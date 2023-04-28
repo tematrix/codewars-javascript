1126c75d2a2f65c76b67c344348738aebc02287c
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-28 13:58:36 +0300

    Mexican wave - description

---
+++ 6 kyo/mexican_wave.js
@@ -0,0 +1,27 @@
+/*
+DESCRIPTION:
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. 
+You will be passed a string and you must return that string in an array where an uppercase 
+letter is a person standing up. 
+
+Rules:
+ 1.  The input string will always be lower case but maybe empty.
+ 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+
+Example:
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+
+ПЕРЕВОД:
+В этом простом задании вам необходимо создать функцию, которая превратит строку в "Mexican Wave".
+На вход функции будет передана строка, и вы должны вернуть эту строку в виде массива, 
+где каждая заглавная буква будет означать, что человек стоит.
+
+Правила:
+
+Входная строка всегда будет в нижнем регистре, но может быть пустой.
+Если символ в строке является пробелом, то пропустите его, как если бы это было пустым местом.
+
+Пример:
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+*/
