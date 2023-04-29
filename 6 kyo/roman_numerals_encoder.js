4f358ba2add32270a945999cbeb09e1125b90775
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-29 13:30:49 +0300

    Roman numerals encoder - description

---
+++ 6 kyo/roman_numerals_encoder.js
@@ -0,0 +1,27 @@
+/*
+DESCRIPTION:
+Create a function taking a positive integer between 1 and 3999 (both included) as its 
+parameter and returning a string containing the Roman Numeral representation of that integer.
+
+Modern Roman numerals are written by expressing each digit separately starting with 
+the left most digit and skipping any digit with a value of zero. 
+In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 
+2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+Example:
+solution(1000); // should return 'M'
+
+
+ПЕРЕВОД:
+Создайте функцию, которая принимает положительное целое число от 1 до 3999 (включительно)
+в качестве параметра и возвращает строку, содержащую римское числовое представление этого числа.
+
+Современные римские цифры записываются, выражая каждую цифру отдельно, начиная с самой левой цифры
+и пропуская любую цифру со значением нуля.
+В римских цифрах 1990 записывается как: 1000=M, 900=CM, 90=XC; в результате получается MCMXC.
+2008 записывается как 2000=MM, 8=VIII; или MMVIII. 1666 использует каждый римский символ в
+убывающем порядке: MDCLXVI.
+
+Пример:
+solution(1000); // должна вернуть 'M'
+*/
