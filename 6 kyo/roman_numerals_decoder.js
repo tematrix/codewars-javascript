24652b276a2ac8113d44298ae52fba5ffb101b5a
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-29 13:37:56 +0300

    Roman numerals decoder - solution

---
+++ 6 kyo/roman_numerals_decoder.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+Create a function that takes a Roman numeral as its argument and returns its value 
+as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the number 
+to be encoded separately, starting with the leftmost digit and skipping any 0s. 
+So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" 
+(2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+Example:
+solution('XXI'); // should return 21
+
+
+ПЕРЕВОД:
+Создайте функцию, которая принимает римское число в качестве аргумента и возвращает его значение
+в виде числового десятичного целого числа. Вам не нужно проверять правильность формы римского числа.
+
+Современные римские цифры записываются, выражая каждую десятичную цифру числа отдельно,
+начиная с самой левой цифры и пропуская любые 0. Так, 1990 записывается как "MCMXC" (1000 = M, 900 = CM, 90 = XC),
+а 2008 записывается как "MMVIII" (2000 = MM, 8 = VIII). Римское число 1666, "MDCLXVI", использует каждую букву в
+убывающем порядке.
+
+Пример:
+solution('XXI'); // должна вернуть 21
+*/
