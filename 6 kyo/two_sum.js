db6fac707733f6bf92156d5a654ffba9a684f762
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-29 13:27:05 +0300

    Two sum - description

---
+++ 6 kyo/two_sum.js
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION:
+Write a function that takes an array of numbers (integers for the tests) and a target number. 
+It should find two different items in the array that, when added together, give the target value. 
+The indices of these items should then be returned in a tuple / list (depending on your language) 
+like so: (index1, index2).
+
+For the purposes of this kata, some tests may have multiple answers; 
+any valid solutions will be accepted.
+
+The input will always be valid (numbers will be an array of length 2 or greater, 
+and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+Based on: http://oj.leetcode.com/problems/two-sum/
+
+twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
+
+
+ПЕРЕВОД:
+Напишите функцию, которая принимает массив чисел (целых чисел для тестов) и целевое число.
+Она должна найти два разных элемента в массиве, которые, когда их сложить, дают целевое значение.
+Индексы этих элементов затем должны быть возвращены в виде кортежа / списка (в зависимости от используемого языка)
+следующим образом: (индекс1, индекс2).
+
+В некоторых тестах может быть несколько верных ответов, все верные решения будут приняты.
+
+Входные данные всегда будут корректными (числа будут массивом длиной 2 или больше,
+и все элементы будут числами; целевое число всегда будет суммой двух разных элементов из этого массива).
+
+На основе: http://oj.leetcode.com/problems/two-sum/
+
+Пример вызова функции:
+twoSum([1, 2, 3], 4) // должна вернуть [0, 2] или [2, 0]
+*/
