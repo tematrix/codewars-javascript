67fc0fc9584592437398a2cc1a0252ab1f6eecd7
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-04-30 12:33:18 +0300

    CamelCase method - description

---
+++ 6 kyo/camelcase_method.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. 
+All words must have their first letter capitalized without spaces.
+
+For instance:
+"hello case".camelCase() => HelloCase
+"camel case word".camelCase() => CamelCaseWord
+
+
+ПЕРЕВОД:
+Напишите простой метод .camelCase (функцию camel_case в PHP, CamelCase в C# или camelCase в Java) для строк.
+Все слова должны начинаться с заглавной буквы без пробелов.
+
+Например:
+"hello case".camelCase() => HelloCase
+"camel case word".camelCase() => CamelCaseWord
+*/
