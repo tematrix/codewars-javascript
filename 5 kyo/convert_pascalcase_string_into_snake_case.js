4a6f6ec3f90250b7c35ef062f259a3fd994f4d89
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-01 13:12:23 +0300

    Сonvert pascalcase string into snake case - description

---
+++ 5 kyo/convert_pascalcase_string_into_snake_case.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+Complete the function/method so that it takes a PascalCase string and returns the string in snake_case notation. 
+Lowercase characters can be numbers. If the method gets a number as input, it should return a string.
+
+Examples
+"TestController"  -->  "test_controller"
+"MoviesAndBooks"  -->  "movies_and_books"
+"App7Test"        -->  "app7_test"
+1                 -->  "1"
+
+
+ПЕРЕВОД:
+Завершите функцию/метод таким образом, чтобы она принимала строку в формате PascalCase и возвращала строку 
+в нотации snake_case. Строчные символы могут быть числами. Если метод получает на вход число, он должен вернуть строку.
+
+Примеры:
+"TestController" --> "test_controller"
+"MoviesAndBooks" --> "movies_and_books"
+"App7Test" --> "app7_test"
+1 --> "1"
+*/
