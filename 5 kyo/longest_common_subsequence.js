2984c2165af7b42a8e04c36eefe7a6dc89d9fc25
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-01 13:20:33 +0300

    Longest common subsequence - description

---
+++ 5 kyo/longest_common_subsequence.js
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION:
+Write a function called LCS that accepts two sequences and returns 
+the longest subsequence common to the passed in sequences.
+
+Subsequence
+A subsequence is different from a substring. The terms of a subsequence 
+need not be consecutive terms of the original sequence.
+
+Example subsequence
+Subsequences of "abc" = "a", "b", "c", "ab", "ac", "bc" and "abc".
+
+LCS examples
+LCS( "abcdef" , "abc" ) => returns "abc"
+LCS( "abcdef" , "acf" ) => returns "acf"
+LCS( "132535365" , "123456789" ) => returns "12356"
+
+
+ПЕРЕВОД:
+Напишите функцию с названием LCS, которая принимает две последовательности и возвращает
+самую длинную общую подпоследовательность переданных последовательностей.
+
+Подпоследовательность
+Подпоследовательность отличается от подстроки. Элементы подпоследовательности
+не обязательно должны быть последовательными элементами исходной последовательности.
+
+Пример подпоследовательности
+Подпоследовательности "abc" = "a", "b", "c", "ab", "ac", "bc" и "abc".
+
+Примеры работы функции LCS
+LCS( "abcdef" , "abc" ) => возвращает "abc"
+LCS( "abcdef" , "acf" ) => возвращает "acf"
+LCS( "132535365" , "123456789" ) => возвращает "12356"
+*/
