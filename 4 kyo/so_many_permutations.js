1dddaa6431c0d9e3bff1cb766d84323afc2a0edc
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-03 11:32:55 +0300

    So many permutations - description

---
+++ 4 kyo/so_many_permutations.js
@@ -0,0 +1,44 @@
+/*
+DESCRIPTION:
+In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present.
+
+Create as many "shufflings" as you can!
+
+Examples:
+
+With input 'a':
+Your function should return: ['a']
+
+With input 'ab':
+Your function should return ['ab', 'ba']
+
+With input 'abc':
+Your function should return ['abc','acb','bac','bca','cab','cba']
+
+With input 'aabb':
+Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+
+Note: The order of the permutations doesn't matter.
+
+
+ПЕРЕВОД:
+В этом ката вам предстоит создать все перестановки непустой входной строки и удалить дубликаты, если они есть.
+
+Создайте как можно больше "перемешиваний"!
+
+Примеры:
+
+Для входной строки 'a':
+Ваша функция должна вернуть: ['a']
+
+Для входной строки 'ab':
+Ваша функция должна вернуть ['ab', 'ba']
+
+Для входной строки 'abc':
+Ваша функция должна вернуть ['abc','acb','bac','bca','cab','cba']
+
+Для входной строки 'aabb':
+Ваша функция должна вернуть ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+
+Примечание: порядок перестановок не имеет значения.
+*/
