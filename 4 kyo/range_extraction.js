64cf29d9cd13aca69994f47ee6bf495245a22de8
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-03 11:12:25 +0300

    Range extraction - solution

--- 4 kyo/range_extraction.js
+++ 4 kyo/range_extraction.js
@@ -28,3 +28,26 @@ solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 1
 solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
 // вернет "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
 */
+
+function solution(list){
+    let result = [], start = list[0], end = '';
+    for (let i = 0; i < list.length; i++) {
+      if (list[i+1]-list[i] === 1) {
+        end = list[i+1];
+      } else if (end !== '' && end - start !== 1) {
+        result.push(`${start}-${end}`);
+        start = list[i+1];
+        end = '';
+      } else if (end !== '') {
+        result.push(`${start},${end}`);
+        start = list[i+1];
+        end = '';
+      } else {
+        result.push(`${start}`);
+        start = list[i+1];
+      }
+    }
+    return result.join(',');  
+}
+
+console.log(solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]));
