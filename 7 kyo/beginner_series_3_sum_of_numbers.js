bc9d5b9f9921dd5711fbcaa532747f5cc17fc9e2
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-04 10:57:00 +0300

    Beginner series 3 sum of numbers - description

---
+++ 7 kyo/beginner_series_3_sum_of_numbers.js
@@ -0,0 +1,35 @@
+/*
+DESCRIPTION:
+Given two integers a and b, which can be positive or negative, find the sum of all the integers 
+between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+Your function should only return a number, not the explanation about how you get that number.
+
+
+ПЕРЕВОД:
+Даны два целых числа a и b, которые могут быть положительными или отрицательными.
+Найдите сумму всех целых чисел между ними, включая их, и верните ее.
+Если два числа равны, верните a или b.
+
+Примечание: a и b не упорядочены!
+
+Примеры (a, b) --> вывод (объяснение)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1, так как оба числа равны)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+Ваша функция должна возвращать только число, а не объяснение, как вы получили это число.
+*/
