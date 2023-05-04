08a627afa8e0cfbefcdd51d1309ebbe83f1cc8a5
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-04 11:03:21 +0300

    Find the next perfect square - description

---
+++ 7 kyo/find_the_next_perfect_square.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION:
+You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. 
+Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+Examples:(Input --> Output)
+
+121 --> 144
+625 --> 676
+114 --> -1 since 114 is not a perfect square
+
+
+ПЕРЕВОД:
+Вы, возможно, знаете несколько довольно крупных квадратов целых чисел. Но что насчет СЛЕДУЮЩЕГО квадрата?
+
+Завершите метод findNextSquare, который находит следующий целочисленный квадрат целого числа после того, 
+который передается в качестве параметра. Напомним, что целочисленный квадрат - это целое число n, 
+такое что квадратный корень из n также является целым числом.
+
+Если переданный параметр сам не является целым квадратом, то должно быть возвращено -1. 
+Можно предположить, что параметр неотрицательный.
+
+Примеры: (ввод --> вывод)
+
+121 --> 144
+625 --> 676
+114 --> -1, так как 114 не является целым квадратом.
+*/
