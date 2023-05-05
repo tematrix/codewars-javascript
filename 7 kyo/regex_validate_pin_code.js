0168100c255e02959a3ee0e3f6a8fc4a24069de2
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-05 11:09:17 +0300

    Regex validate pin code - description

---
+++ 7 kyo/regex_validate_pin_code.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything 
+but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+
+Examples (Input --> Output)
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+
+
+ПЕРЕВОД:
+Банкоматы позволяют использовать PIN-коды длиной 4 или 6 цифр, и PIN-коды не могут содержать ничего, 
+кроме ровно 4 цифр или ровно 6 цифр.
+
+Если функции передается действительная строка PIN-кода, вернуть true, иначе вернуть false.
+
+Примеры (ввод --> вывод)
+"1234" --> true
+"12345" --> false
+"a234" --> false
+*/
