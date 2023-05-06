96e9a891439bc339fa42ddf8efa51fb0f64f63e5
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-06 13:29:31 +0300

    String ends with - description

---
+++ 7 kyo/string_ends_with.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Complete the solution so that it returns true if the first argument(string) 
+passed in ends with the 2nd argument (also a string).
+
+Examples:
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+
+ПЕРЕВОД:
+Завершите решение так, чтобы оно возвращало true, если первый аргумент (строка),
+заканчивается на второй аргумент (также строку).
+
+Примеры:
+solution('abc', 'bc') // возвращает true
+solution('abc', 'd') // возвращает false
+*/
