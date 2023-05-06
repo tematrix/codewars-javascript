a9c93ae626bee47b49af3083650a48716ec79ae2
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-06 13:39:46 +0300

    Odd or even - description

---
+++ 7 kyo/odd_or_even.js
@@ -0,0 +1,36 @@
+/*
+DESCRIPTION:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+
+
+ПЕРЕВОД:
+Дан список целых чисел. Определите, является ли сумма его элементов четной или нечетной.
+
+Верните строку "even" (четная) или "odd" (нечетная).
+
+Если входной массив пустой, то его нужно считать равным [0] (содержит один элемент равный нулю).
+
+Примеры:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+*/
