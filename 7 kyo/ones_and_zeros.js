3260e8b4de4bf3fb3a2c9129537f82d1a9d3fc88
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-06 13:21:50 +0300

    Ones and zeros - description

---
+++ 7 kyo/ones_and_zeros.js
@@ -0,0 +1,36 @@
+/*
+DESCRIPTION:
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+
+However, the arrays can have varying lengths, not just limited to 4.
+
+
+ПЕРЕВОД:
+Дан массив из нулей и единиц, нужно преобразовать его в соответствующее целое число, которое представляет собой двоичное значение.
+
+Например, массив [0, 0, 0, 1] представляет собой двоичное число 0001, которое равно 1.
+
+Примеры:
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+
+Однако, длина массивов может быть различной, а не ограничиваться только 4 элементами.
+*/
