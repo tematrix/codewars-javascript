02ba10e79b7839b9758b8822ba66b7484837315e
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-06 13:41:15 +0300

    Odd or even - solution

--- 7 kyo/odd_or_even.js
+++ 7 kyo/odd_or_even.js
@@ -34,3 +34,16 @@ Output: "odd"
 Input: [0, -1, -5]
 Output: "even"
 */
+
+function oddOrEven(array) {
+    if (!array) {return 'even';}
+    let sum = 0;
+    for (let e of array) {
+      sum += e;
+    }
+    return sum % 2 === 0 ? "even" : "odd";
+}
+
+console.log(oddOrEven([0]));
+console.log(oddOrEven([0, 1, 4]));
+console.log(oddOrEven([0, -1, -5]));
