ac78170202535a7d782964a1b4156383850d3be3
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-07 15:09:40 +0300

    Reverse words - description

---
+++ 7 kyo/reverse_words.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Complete the function that accepts a string parameter, and reverses each word in the string. 
+All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+
+
+ПЕРЕВОД:
+Задача заключается в написании функции, которая принимает на вход 
+строку и переворачивает каждое слово в этой строке. 
+При этом все пробелы в исходной строке должны быть сохранены на своих местах.
+
+Например, строка "This is an example!" должна быть преобразована в строку "sihT si na !elpmaxe", 
+а строка "double spaces" должна стать "elbuod secaps".
+*/
