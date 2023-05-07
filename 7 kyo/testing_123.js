8cfd73de561c72546687b1f786aa64aa98085455
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-07 15:20:12 +0300

    Testing 123 - solution

--- 7 kyo/testing_123.js
+++ 7 kyo/testing_123.js
@@ -25,3 +25,10 @@ Examples: (Input --> Output)
 [] --> []
 ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
 */
+
+var number=function(array){
+    return !array ? [] : array.map((e,i) => `${i+1}: ${e}`); 
+};
+
+console.log(number([]));
+console.log(number(["a", "b", "c"]));
