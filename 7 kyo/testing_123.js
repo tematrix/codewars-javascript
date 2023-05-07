7b8ea6b612ec79d6847b2b06970fe3349f707383
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-07 15:18:48 +0300

    Testing 123 - description

---
+++ 7 kyo/testing_123.js
@@ -0,0 +1,27 @@
+/*
+DESCRIPTION:
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+
+ПЕРЕВОД:
+Ваша команда разрабатывает новый текстовый редактор, и вам поручено реализовать нумерацию строк.
+
+Напишите функцию, которая принимает список строк и возвращает каждую строку с правильным номером в начале.
+
+Нумерация начинается с 1. Формат строки: номер: строка. Обратите внимание на двоеточие 
+и пробел между ним и текстом строки.
+
+Примеры: (Вход --> Выход)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+*/
