1c26395a07a658577c9744375bb77e8de21f067c
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-08 14:57:30 +0300

    Find the stray number - description

---
+++ 7 kyo/find_the_stray_number.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+
+
+ПЕРЕВОД:
+Вам дан массив целых чисел нечетной длины, в котором все они одинаковы, за исключением одного числа.
+
+Завершите метод, который принимает такой массив и возвращает это единственное различное число.
+
+Входной массив всегда будет допустимым! (нечетная длина >= 3)
+
+Примеры
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+*/
