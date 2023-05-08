bb774b22c2a21746172cb2ef0458aec5a786771b
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-08 14:40:34 +0300

    The highest profit wins - description

---
+++ 7 kyo/the_highest_profit_wins.js
@@ -0,0 +1,35 @@
+/*
+DESCRIPTION:
+Ben has a very simple idea to make some profit: he buys something and sells it again. 
+Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. 
+nstead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+Task
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+Examples (Input --> Output)
+[1,2,3,4,5] --> [1,5]
+[2334454,5] --> [5,2334454]
+[1]         --> [1,1]
+Remarks
+All arrays or lists will always have at least one element, so you don't need to check the length. 
+Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+
+ПЕРЕВОД:
+Бен имеет очень простую идею, как заработать некоторую прибыль: он покупает что-то и продает это снова. 
+Конечно, это не даст ему никакой прибыли, если он просто будет покупать и продавать по одной и той же цене. 
+Вместо этого он собирается купить это за самую низкую возможную цену и продать за самую высокую.
+
+Задача
+Напишите функцию, которая возвращает как минимум, так и максимум чисел из данного списка/массива.
+
+Примеры (ввод --> вывод)
+[1,2,3,4,5] --> [1,5]
+[2334454,5] --> [5,2334454]
+[1] --> [1,1]
+
+Примечания
+Все массивы или списки всегда будут иметь как минимум один элемент, так что вам не нужно проверять длину. 
+Кроме того, ваша функция всегда получит массив или список, так что вам не нужно проверять на null, undefined или подобное.
+*/
