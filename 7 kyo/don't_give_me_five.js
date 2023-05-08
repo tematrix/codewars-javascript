29cdace7b6b0ca3047ccd40910a0eb6aaae08d6b
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-08 14:55:05 +0300

    Don't give me five - solution

--- 7 kyo/don't_give_me_five.js
+++ 7 kyo/don't_give_me_five.js
@@ -18,3 +18,16 @@ Examples:
 1,9 -> 1,2,3,4,6,7,8,9 -> Результат 8
 4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Результат 12
 */
+
+function dontGiveMeFive(start, end) {
+    let result = 0;
+    for (let i = start; i <= end; i++) {
+      if (!String(i).includes('5')) {
+        result++;
+      }
+    }
+    return result;
+}
+
+console.log(dontGiveMeFive(1,9));
+console.log(dontGiveMeFive(4,17));
