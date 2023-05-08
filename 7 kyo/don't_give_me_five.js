360c0a24d1a4fef73281f519925e78675e20347e
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-08 14:53:41 +0300

    Don't give me five - description

---
+++ 7 kyo/don't_give_me_five.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+In this kata you get the start number and the end number of a region and should return 
+the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+Examples:
+
+1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+
+
+ПЕРЕВОД:
+В этом ката вам дается начальное число и конечное число диапазона, и вы должны вернуть количество 
+всех чисел, за исключением чисел, содержащих цифру 5. Начальное и конечное число включаются в диапазон!
+
+Примеры:
+
+1,9 -> 1,2,3,4,6,7,8,9 -> Результат 8
+4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Результат 12
+*/
