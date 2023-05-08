9edcd36dfa6e6a2eae43bce882aa940a3b4d3570
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-08 14:58:42 +0300

    Find the stray number - solution

--- 7 kyo/find_the_stray_number.js
+++ 7 kyo/find_the_stray_number.js
@@ -22,3 +22,11 @@ Examples
 [1, 1, 2] ==> 2
 [17, 17, 3, 17, 17, 17, 17] ==> 3
 */
+
+function stray(numbers) {
+    numbers.sort((a,b) => a - b);
+    return numbers[0] === numbers[1] ? numbers[numbers.length-1] : numbers[0];
+}
+
+console.log(stray([1, 1, 2]));
+console.log(stray([17, 17, 3, 17, 17, 17, 17]));
