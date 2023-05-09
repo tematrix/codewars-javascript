23da4f56573b83bf6ee4b86852b9291e3e07cb8c
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-09 13:39:08 +0300

    Multiplication table - description

---
+++ 6 kyo/multiplication_table.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+Your task, is to create N×N multiplication table, of size provided in parameter.
+
+For example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+
+For the given example, the return value should be:
+
+[[1,2,3],[2,4,6],[3,6,9]]
+
+
+ПЕРЕВОД:
+Ваша задача состоит в том, чтобы создать таблицу умножения размером N × N, где N - число, заданное в параметре.
+
+Например, при заданном размере 3:
+
+1 2 3
+2 4 6
+3 6 9
+
+Для данного примера возвращаемое значение должно быть:
+
+[[1,2,3],[2,4,6],[3,6,9]]
+*/
