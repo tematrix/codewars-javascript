934bcae731d56f5888f9c649ea64eb4f9c8b4c40
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-09 13:50:23 +0300

    Help the bookseller - solution

--- 6 kyo/help_the_bookseller.js
+++ 6 kyo/help_the_bookseller.js
@@ -62,3 +62,20 @@ M = ["A", "B", "C", "W"] или ...
 
 Если L или M пусты, то возвращаемая строка - "" (в Clojure/Racket/Prolog следует возвращать пустой массив/список).
 */
+
+function stockList(listOfArt, listOfCat){
+    if (listOfArt.length === 0 || listOfCat.length === 0) {return '';}
+    let result = [];
+    for (let i = 0; i < listOfCat.length; i++) {
+      let sum = 0;
+      for (let j = 0; j < listOfArt.length; j++) {
+        if (listOfArt[j][0] === listOfCat[i]) {       
+          sum += +listOfArt[j].match(/\d+$/);
+        }
+      }
+      result.push(`(${listOfCat[i]} : ${sum})`);
+    }
+    return result.join(' - ');
+}
+
+console.log(stockList(["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"],["A", "B", "C", "W"]));
