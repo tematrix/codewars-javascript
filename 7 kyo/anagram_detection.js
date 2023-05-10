bcb59ca5db34b5c07ba3c7d8fc347d85fb3eaf6b
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-10 10:57:55 +0300

    Anagram detection - description

---
+++ 7 kyo/anagram_detection.js
@@ -0,0 +1,27 @@
+/*
+DESCRIPTION:
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; 
+return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"
+
+
+ПЕРЕВОД:
+Анаграмма - это результат перестановки букв слова для получения нового слова (см. Википедию).
+
+Примечание: анаграммы не зависят от регистра букв.
+
+Завершите функцию, чтобы вернуть true, если два заданных аргумента являются анаграммами друг друга; 
+в противном случае вернуть false.
+
+Примеры:
+"foefet" является анаграммой "toffee"
+"Buckethead" является анаграммой "DeathCubeK"
+*/
