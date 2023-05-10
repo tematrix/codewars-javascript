adac1cc279d4c3de4f849734fd9464738b44a594
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-10 11:02:02 +0300

    Factorial - description

---
+++ 7 kyo/factorial.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+In mathematics, the factorial of a non-negative integer n, denoted by n!, 
+is the product of all positive integers less than or equal to n. 
+For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+
+Write a function to calculate factorial for a given input. 
+If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) 
+or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) 
+or ValueError (Python) or return -1 (C).
+
+
+ПЕРЕВОД:
+В математике факториал неотрицательного целого числа n, обозначаемый n!,
+является произведением всех положительных целых чисел, меньших или равных n.
+Например: 5! = 5 * 4 * 3 * 2 * 1 = 120. По соглашению значение 0! равно 1.
+
+Напишите функцию для вычисления факториала заданного числа.
+Если входное значение меньше 0 или больше 12, выбросьте исключение типа ArgumentOutOfRangeException (C#)
+или IllegalArgumentException (Java) или RangeException (PHP) или выбросьте RangeError (JavaScript)
+или ValueError (Python) или верните -1 (C).
+*/
