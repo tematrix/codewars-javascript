6c27254cb8ca30801cbe9ff747b065edfdfaaab9
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-10 11:03:19 +0300

    Factorial - solution

--- 7 kyo/factorial.js
+++ 7 kyo/factorial.js
@@ -20,3 +20,14 @@ or ValueError (Python) or return -1 (C).
 или IllegalArgumentException (Java) или RangeException (PHP) или выбросьте RangeError (JavaScript)
 или ValueError (Python) или верните -1 (C).
 */
+
+function factorial(n) {
+    if (n < 0 || n > 12) {throw new RangeError;}
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * factorial(n-1);
+    }
+}
+
+console.log(factorial(5));
