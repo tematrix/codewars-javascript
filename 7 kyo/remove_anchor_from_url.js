c0acf2724c173325304a524716eb3bf90911e925
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-11 15:24:55 +0300

    Remove anchor from url - description

---
+++ 7 kyo/remove_anchor_from_url.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+
+ПЕРЕВОД:
+Напишите функцию/метод, который возвращает URL без фрагмента после символа "#".
+
+Примеры:
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+*/
