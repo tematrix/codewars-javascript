f530990a808c46c6d8b1c748365dfa7299a555ee
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-11 15:28:55 +0300

    Small enough beginner - description

---
+++ 7 kyo/small_enough_beginner.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+You will be given an array and a limit value. 
+You must check that all values in the array are below or equal to the limit value. 
+If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+
+
+ПЕРЕВОД:
+Дан массив и значение лимита. 
+Необходимо проверить, что все значения в массиве меньше или равны лимиту. 
+Если все значения меньше или равны лимиту, вернуть true, иначе - false.
+
+Можно предположить, что все значения в массиве являются числами.
+*/
