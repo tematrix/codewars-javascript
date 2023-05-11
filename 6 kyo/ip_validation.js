a80a1b9b8a778284e31edda79965a77da526177a
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-11 15:21:15 +0300

    Ip validation - description

---
+++ 6 kyo/ip_validation.js
@@ -0,0 +1,40 @@
+/*
+DESCRIPTION:
+Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. 
+IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
+
+Valid inputs examples:
+
+Examples of valid inputs:
+1.2.3.4
+123.45.67.89
+
+Invalid input examples:
+1.2.3
+1.2.3.4.5
+123.456.78.90
+123.045.067.089
+
+Notes:
+Leading zeros (e.g. 01.02.03.04) are considered invalid
+Inputs are guaranteed to be a single string
+
+
+ПЕРЕВОД:
+Напишем алгоритм для определения корректных IPv4-адресов в десятично-точечном формате. 
+Адреса будут считаться корректными, если они состоят из четырех октетов со значениями от 0 до 255 включительно.
+
+Примеры корректных адресов:
+1.2.3.4
+123.45.67.89
+
+Примеры некорректных адресов:
+1.2.3
+1.2.3.4.5
+123.456.78.90
+123.045.067.089
+
+Примечания:
+Ведущие нули (например, 01.02.03.04) считаются некорректными
+Входные данные гарантированно являются одной строкой.
+*/
