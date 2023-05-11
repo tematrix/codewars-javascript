a3e7e8ce3dd462796a087685158ebecd3520d589
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-11 15:26:10 +0300

    Remove anchor from url - solution

--- 7 kyo/remove_anchor_from_url.js
+++ 7 kyo/remove_anchor_from_url.js
@@ -14,3 +14,10 @@ Examples
 "www.codewars.com#about" --> "www.codewars.com"
 "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 */
+
+function removeUrlAnchor(url){
+    return url.split('#')[0];
+}
+
+console.log(removeUrlAnchor("www.codewars.com#about"));
+console.log(removeUrlAnchor("www.codewars.com?page=1"));
