f563ee0d9dc30598a58058809b8a5cc4d81b8fc7
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-12 13:53:34 +0300

    Summing a number's digits - description

---
+++ 7 kyo/summing_a_number's_digits.js
@@ -0,0 +1,25 @@
+/*
+DESCRIPTION:
+Write a function named sumDigits which takes a number as input and returns 
+the sum of the absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+Let's assume that all numbers in the input will be integer values.
+
+
+ПЕРЕВОД:
+Напишите функцию с названием sumDigits, которая принимает число в качестве входного 
+параметра и возвращает сумму абсолютных значений каждой десятичной цифры числа.
+
+Например: (Входные данные --> Выходные данные)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+
+Предполагается, что все числа во входных данных будут целыми значениями.
+*/
