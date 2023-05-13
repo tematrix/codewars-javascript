5904cdb4fa646c3cec3926f2d6949675f9398bae
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-13 13:41:14 +0300

    No oddities here - description

---
+++ 7 kyo/no_oddities_here.js
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given.
+
+
+ПЕРЕВОД:
+Напишите небольшую функцию, которая возвращает значения массива, которые не являются нечетными.
+
+Все значения в массиве будут целыми числами. Верните хорошие значения в том порядке, в котором они даны.
+*/
