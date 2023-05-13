fdb2c07a078fcd65900e997bb5356ea9ccb4719e
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-13 13:37:00 +0300

    Flatten and sort an array - description

---
+++ 7 kyo/flatten_and_sort_an_array.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+Given a two-dimensional array of integers, return the flattened version 
+of the array with all the integers in the sorted (ascending) order.
+
+Example:
+Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+
+ПЕРЕВОД:
+Дан двухмерный массив целых чисел. Необходимо вернуть "сплющенную" (одномерную) версию массива, 
+содержащую все целые числа в отсортированном (возрастающем) порядке.
+
+Пример:
+Дан массив [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]. Ваша функция должна вернуть [1, 2, 3, 4, 5, 6, 7, 8, 9].
+*/
