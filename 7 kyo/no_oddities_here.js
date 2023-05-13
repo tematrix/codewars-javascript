86bb2f1b887882a1f9d39f89afcfb94bbacb35f1
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-13 13:42:30 +0300

    No oddities here - solution

--- 7 kyo/no_oddities_here.js
+++ 7 kyo/no_oddities_here.js
@@ -10,3 +10,9 @@ All values in the array will be integers. Return the good values in the order th
 
 Все значения в массиве будут целыми числами. Верните хорошие значения в том порядке, в котором они даны.
 */
+
+function noOdds(values){
+    return values.filter(e => e % 2 === 0);
+}
+
+console.log(noOdds([1,2,3,4,5,6,7,8,9]));
