5ce7fb54bb86ff8b6c613848cc8326f14191b488
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-14 13:35:09 +0300

    Sum of minimums - description

---
+++ 7 kyo/sum_of_minimums.js
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION:
+Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, 
+your task is to find the sum of the minimum values in each row.
+
+For Example:
+
+[ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+, [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+, [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+]
+So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+Note: You will always be given a non-empty list containing positive values.
+
+
+ПЕРЕВОД:
+Дана двумерная (вложенная) матрица (список, массив и т. д.) размером m * n.
+Ваша задача - найти сумму минимальных значений в каждой строке.
+
+Например:
+
+[ [ 1, 2, 3, 4, 5 ] # минимальное значение в строке - 1
+, [ 5, 6, 7, 8, 9 ] # минимальное значение в строке - 5
+, [ 20, 21, 34, 56, 100 ] # минимальное значение в строке - 20
+]
+Функция должна вернуть 26, потому что сумма минимумов равна 1 + 5 + 20 = 26.
+
+Примечание: Вам всегда будет дан не пустой список, содержащий положительные значения.
+*/
