d89f7ed166a4417070b3726722f601050964fadb
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-15 10:46:04 +0300

    Greet me - solution

--- 7 kyo/greet_me.js
+++ 7 kyo/greet_me.js
@@ -18,3 +18,10 @@ Example:
 "riley" --> "Привет, Райли!"
 "JACK" --> "Привет, Джек!"
 */
+
+const greet = function(name) {
+    return `Hello ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`;
+};
+
+console.log(greet("riley"));
+console.log(greet("JACK"));
