c8a81205fa128c2dd0ea00c7e7e806bf32be9b4a
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-15 10:49:05 +0300

    Meeting - description

---
+++ 6 kyo/meeting.js
@@ -0,0 +1,35 @@
+/*
+DESCRIPTION:
+John has invited some friends. His list is:
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+Could you make a program that
+
+makes this string uppercase
+gives it sorted in alphabetical order by last name.
+When the last names are the same, sort them by first name. 
+Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+So the result of function meeting(s) will be:
+
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+It can happen that in two distinct families with the same family name two people have the same first name too.
+
+
+ПЕРЕВОД:
+Джон пригласил несколько друзей. Его список выглядит так:
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+
+Можете ли вы создать программу, которая:
+
+Приводит эту строку к верхнему регистру.
+Сортирует список в алфавитном порядке по фамилии.
+При одинаковых фамилиях, сортирует их по имени.
+Фамилия и имя гостя выводятся в результате в скобках, разделенные запятой.
+Таким образом, результат функции meeting(s) будет выглядеть так:
+
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+
+Может случиться, что в двух разных семьях с одинаковой фамилией два человека также имеют одно и то же имя.
+*/
