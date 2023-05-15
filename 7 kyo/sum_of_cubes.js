a48e4b76653bb67daafe73bc54c8e54af133427b
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-15 10:58:35 +0300

    Sum of cubes - solution

--- 7 kyo/sum_of_cubes.js
+++ 7 kyo/sum_of_cubes.js
@@ -22,3 +22,14 @@ Examples: (Input --> output)
 2 --> 9 (сумма кубов 1 и 2 равна 1 + 8)
 3 --> 36 (сумма кубов 1, 2 и 3 равна 1 + 8 + 27)
 */
+
+function sumCubes(n){
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i**3;
+    }
+    return sum;
+}
+
+console.log(sumCubes(2));
+console.log(sumCubes(3));
