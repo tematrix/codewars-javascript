5a7e70be967805680fcaba4a47df697c8d6108f1
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-15 10:44:52 +0300

    Greet me - description

---
+++ 7 kyo/greet_me.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+Write a method that takes one argument as name and then greets that name, 
+capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+
+
+ПЕРЕВОД:
+Напишите метод, который принимает один аргумент в качестве имени, а затем приветствует это имя, 
+написанное с заглавной буквы и заканчивающееся восклицательным знаком.
+
+Пример:
+
+"riley" --> "Привет, Райли!"
+"JACK" --> "Привет, Джек!"
+*/
