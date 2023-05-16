a1d5268e2baf5590a57e3b2f596763f9139d8d8e
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-16 21:29:29 +0300

    The vowel code - description

---
+++ 6 kyo/the_vowel_code.js
@@ -0,0 +1,42 @@
+/*
+DESCRIPTION:
+Step 1: Create a function called encode() to replace all the lowercase vowels 
+in a given string with numbers according to the following pattern:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+For example, encode("hello") would return "h2ll4". 
+There is no need to worry about uppercase vowels in this kata.
+
+Step 2: Now create a function called decode() to turn the numbers back 
+into vowels according to the same pattern shown above.
+
+For example, decode("h3 th2r2") would return "hi there".
+
+For the sake of simplicity, you can assume that any numbers 
+passed into the function will correspond to vowels.
+
+
+ПЕРЕВОД:
+Шаг 1: Создайте функцию с названием encode(), которая заменит все строчные гласные 
+в заданной строке цифрами в соответствии с следующим шаблоном:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+Например, encode("hello") вернет "h2ll4". 
+В этом ката нет необходимости заботиться о заглавных гласных.
+
+Шаг 2: Теперь создайте функцию с названием decode(), чтобы преобразовать цифры 
+обратно в гласные в соответствии с тем же шаблоном, показанным выше.
+
+Например, decode("h3 th2r2") вернет "hi there".
+
+Для простоты можно предположить, что любые цифры, переданные в функцию, 
+будут соответствовать гласным.
+*/
