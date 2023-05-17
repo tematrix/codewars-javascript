60501fdf25e9461eb84f2fb6e26aaa3887f76d11
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-17 23:18:41 +0300

    Pyramid array - description

---
+++ 6 kyo/pyramid_array.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+pyramid(0) => [ ]
+pyramid(1) => [ [1] ]
+pyramid(2) => [ [1], [1, 1] ]
+pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+
+Note: the subarrays should be filled with 1s
+
+
+ПЕРЕВОД:
+Напишите функцию, которая при заданном числе >= 0 возвращает массив подмассивов возрастающей длины.
+
+pyramid(0) => [ ]
+pyramid(1) => [ [1] ]
+pyramid(2) => [ [1], [1, 1] ]
+pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+
+Примечание: подмассивы должны быть заполнены единицами.
+*/
