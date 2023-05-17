accf30a4e554ed8355219b12181ee116482af7db
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-17 23:14:35 +0300

    Love vs friendship - description

---
+++ 7 kyo/love_vs_friendship.js
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION:
+If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice as strong as love :-)
+
+Your task is to write a function which calculates the value of a word 
+based off the sum of the alphabet positions of its characters.
+
+The input will always be made of only lowercase letters and will never be empty.
+
+
+ПЕРЕВОД:
+Если a = 1, b = 2, c = 3 ... z = 26
+
+Тогда l + o + v + e = 54
+
+и f + r + i + e + n + d + s + h + i + p = 108
+
+Значит, дружба в два раза сильнее любви :-)
+
+Ваша задача - написать функцию, которая вычисляет значение слова 
+на основе суммы позиций букв в алфавите.
+
+Входные данные всегда будут состоять только из строчных букв и никогда не будут пустыми.
+*/
