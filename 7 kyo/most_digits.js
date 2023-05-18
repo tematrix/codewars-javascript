16100408793b879f9f1544d62c425d001bf523ce
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-18 12:24:21 +0300

    Most digits - description

---
+++ 7 kyo/most_digits.js
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Find the number with the most digits.
+
+If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+
+ПЕРЕВОД:
+Найти число с наибольшим количеством цифр.
+
+Если два числа в исходном массиве имеют одинаковое количество цифр, вернуть первое из них в массиве.
+*/
