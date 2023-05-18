aa8e366e4e22c054c8ea42d4853330d2246507f4
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-18 12:15:18 +0300

    Sort arrays 1 - description

---
+++ 7 kyo/sort_arrays_1.js
@@ -0,0 +1,9 @@
+/*
+DESCRIPTION:
+Just a simple sorting usage. Create a function that returns 
+the elements of the input-array / list sorted in lexicographical order.
+
+
+ПЕРЕВОД:
+Создайте функцию, которая возвращает элементы входного массива / списка, отсортированные в лексикографическом порядке.
+*/
