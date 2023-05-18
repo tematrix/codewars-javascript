b58442a848949834e2408cb90e58b1e05f959e9e
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-18 12:25:52 +0300

    Most digits - solution

--- 7 kyo/most_digits.js
+++ 7 kyo/most_digits.js
@@ -10,3 +10,15 @@ If two numbers in the argument array have the same number of digits, return the 
 
 Если два числа в исходном массиве имеют одинаковое количество цифр, вернуть первое из них в массиве.
 */
+
+function findLongest(array){
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (String(array[i]).length > String(max).length) {
+        max = array[i];
+      }
+    }
+    return max;
+}
+
+console.log(findLongest([1,123,34,5555,14,345,78]));
