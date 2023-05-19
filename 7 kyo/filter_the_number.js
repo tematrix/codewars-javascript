e13328f18170054ef39c7e62b796954b25c0c49f
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-19 18:15:26 +0300

    Filter the number - description

---
+++ 7 kyo/filter_the_number.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+Oh, no! The number has been mixed up with the text. 
+Your goal is to retrieve the number from the text, can you return the number back to its original state?
+
+Task
+Your task is to return a number from a string.
+
+Details
+You will be given a string of numbers and letters mixed up, 
+you have to return all the numbers in that string in the order they occur.
+
+
+ПЕРЕВОД:
+О нет! Число было перемешано с текстом.
+Ваша задача - извлечь число из текста. Можете ли вы вернуть число в его исходное состояние?
+
+Задача
+Вам нужно вернуть число из строки.
+
+Детали
+Вам будет дана строка, в которой числа и буквы перемешаны. 
+Вам нужно вернуть все числа в том порядке, в котором они встречаются в строке.
+*/
