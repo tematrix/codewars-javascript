38206fc4521068bb6c7ee15bd31255262a70b44f
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-19 18:10:25 +0300

    Dashatize it - description

---
+++ 6 kyo/dashatize_it.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+Given a variable n,
+If n is an integer, Return a string with dash'-'marks before and after each odd integer, 
+but do not begin or end the string with a dash mark. If n is negative, then the negative sign should be removed.
+
+If n is not an integer, return the string "NaN".
+
+Ex:
+dashatize(274) -> '2-7-4'
+dashatize(6815) -> '68-1-5'
+
+
+ПЕРЕВОД:
+Дана переменная n.
+Если n - целое число, вернуть строку с дефисными знаками '-' перед и после каждого нечетного целого числа, 
+но не начинать и не заканчивать строку дефисом. Если n отрицательное, то знак минус должен быть удален.
+
+Если n не является целым числом, вернуть строку "NaN".
+
+Пример:
+dashatize(274) -> '2-7-4'
+dashatize(6815) -> '68-1-5'
+*/
