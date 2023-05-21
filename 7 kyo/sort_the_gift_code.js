3d556871e32227d958c126b2b455c6f972aa828a
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-21 20:59:37 +0300

    Sort the gift code - description

---
+++ 7 kyo/sort_the_gift_code.js
@@ -0,0 +1,44 @@
+/*
+DESCRIPTION:
+Happy Holidays fellow Code Warriors!
+Santa's senior gift organizer Elf developed a way to represent up 
+to 26 gifts by assigning a unique alphabetical character to each gift. 
+After each gift was assigned a character, the gift organizer Elf then 
+joined the characters to form the gift ordering code.
+
+Santa asked his organizer to order the characters in alphabetical order, 
+but the Elf fell asleep from consuming too much hot chocolate and candy canes! 
+Can you help him out?
+
+Sort the Gift Code
+Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts 
+a string containing up to 26 unique alphabetical characters, and returns a string 
+containing the same characters in alphabetical order.
+
+Examples (Input -- => Output):
+"abcdef"                      -- => "abcdef"
+"pqksuvy"                     -- => "kpqsuvy"
+"zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+
+
+ПЕРЕВОД:
+Счастливых праздников, коллеги-разработчики!
+
+Старший организатор подарков эльфа разработал способ представления до 26 подарков,
+назначив каждому подарку уникальную букву алфавита. После того, как каждому подарку
+была назначена буква, организатор подарков объединил буквы, чтобы создать код
+порядка подарков.
+
+Санта попросил организатора отсортировать буквы в алфавитном порядке,
+но эльф уснул после употребления слишком много горячего шоколада и карамелек!
+Можете ли вы ему помочь?
+
+Напишите функцию с названием sortGiftCode/sort_gift_code/SortGiftCode,
+которая принимает строку, содержащую до 26 уникальных букв алфавита,
+и возвращает строку, содержащую те же символы в алфавитном порядке.
+
+Примеры (ввод -- => вывод):
+"abcdef" -- => "abcdef"
+"pqksuvy" -- => "kpqsuvy"
+"zyxwvutsrqponmlkjihgfedcba" -- => "abcdefghijklmnopqrstuvwxyz"
+*/
