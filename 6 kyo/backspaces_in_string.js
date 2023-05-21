35a7b50eefd42e9cdfdf332e2825f74fb8b51ad8
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-21 20:53:49 +0300

    Backspaces in string - description

---
+++ 6 kyo/backspaces_in_string.js
@@ -0,0 +1,25 @@
+/*
+DESCRIPTION:
+Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+Your task is to process a string with "#" symbols.
+
+Examples
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+
+
+ПЕРЕВОД:
+Предположим, что символ "#" в строке действует как клавиша "Backspace" на клавиатуре. 
+Это означает, что строка "a#bc#d" фактически будет выглядеть как "bd".
+
+Ваша задача - обработать строку, содержащую символы "#".
+
+Примеры:
+"abc#d##c" ==> "ac"
+"abc##d######" ==> ""
+"#######" ==> ""
+"" ==> ""
+*/
