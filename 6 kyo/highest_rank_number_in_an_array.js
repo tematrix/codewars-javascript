ee30ff180beaef3202e8b279ccc902e7c8546f30
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-21 21:07:43 +0300

    Highest rank number in an array - solution

--- 6 kyo/highest_rank_number_in_an_array.js
+++ 6 kyo/highest_rank_number_in_an_array.js
@@ -22,3 +22,19 @@ Examples
 [12, 10, 8, 12, 7, 6, 4, 10, 12, 10] --> 12
 [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10] --> 3
 */
+
+function highestRank(arr){
+    let o = {};
+    for (let e of arr) {o.hasOwnProperty(e) ? o[e]++ : o[e] = 1;}
+    
+    let a = Object.entries(o),
+        b = a.sort((x,y) => y[1] - x[1])
+             .filter(i => i[1] === a[0][1])
+             .sort((x,y) => y[0] - x[0]);
+    
+    return Number(b[0][0]);
+}
+
+console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]));
+console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10]));
+console.log(highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]));
