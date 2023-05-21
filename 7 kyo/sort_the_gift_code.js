04098439ba8a3b610a75dfc1e44f97d33560b761
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-21 21:01:14 +0300

    Sort the gift code - solution

--- 7 kyo/sort_the_gift_code.js
+++ 7 kyo/sort_the_gift_code.js
@@ -42,3 +42,11 @@ Examples (Input -- => Output):
 "pqksuvy" -- => "kpqsuvy"
 "zyxwvutsrqponmlkjihgfedcba" -- => "abcdefghijklmnopqrstuvwxyz"
 */
+
+function sortGiftCode(code){
+    return code.split('').sort().join(''); 
+}
+
+console.log(sortGiftCode("abcdef"));
+console.log(sortGiftCode("pqksuvy"));
+console.log(sortGiftCode("zyxwvutsrqponmlkjihgfedcba"));
