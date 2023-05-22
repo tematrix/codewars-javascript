5d8833bb32c338ce1e99c61d3673b585ca0f61cc
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-22 22:12:59 +0300

    Building strings from a hash - description

---
+++ 7 kyo/building_strings_from_a_hash.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+Complete the solution so that it takes the object (JavaScript/CoffeeScript) 
+or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+The format should be "KEY = VALUE". Each key/value pair 
+should be separated by a comma except for the last pair.
+
+Example:
+
+solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+
+
+ПЕРЕВОД:
+Завершите решение так, чтобы оно принимало переданный объект (JavaScript/CoffeeScript) 
+или хэш (Ruby) и генерировало строку, читаемую человеком, из его пар ключ/значение.
+
+Формат должен быть "KEY = VALUE". Каждая пара ключ/значение 
+должна быть разделена запятой, за исключением последней пары.
+
+Пример:
+
+solution({a: 1, b: '2'}) // должно вернуть "a = 1, b = 2"
+*/
