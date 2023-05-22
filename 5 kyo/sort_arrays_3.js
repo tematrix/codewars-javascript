a7932e858958026224756a72b6e272b3605f322a
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-22 22:26:29 +0300

    Sort arrays 3 - solution

--- 5 kyo/sort_arrays_3.js
+++ 5 kyo/sort_arrays_3.js
@@ -14,3 +14,15 @@ The return of the function shall first be sorted by yymm, then by the name (whic
 Возвращаемое значение функции должно быть отсортировано сначала по ггмм, 
 а затем по названию (которое может иметь разную длину).
 */
+
+const sortme = function(courses) {
+    let a = [];
+    for (let e of courses) {a.push(e.split('-'));}
+    return a.sort((x,y) => x[1] - y[1]).sort((x,y) => {
+      if (x[1] === y[1]) {
+        if (x[0] > y[0]) {return 1;}
+        if (x[0] < y[0]) {return -1;}
+        if (x[0] === y[0]) {return 0;}
+      }
+    }).map(e => `${e[0]}-${e[1]}`);
+};
