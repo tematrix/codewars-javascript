550658a3509ac8dc7fc261990238ab84534d86f4
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-22 22:24:56 +0300

    Sort arrays 3 - description

---
+++ 5 kyo/sort_arrays_3.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+This time the input is a sequence of course-ids that are formatted in the following way:
+
+name-yymm
+The return of the function shall first be sorted by yymm, then by the name (which varies in length).
+
+
+ПЕРЕВОД:
+На этот раз входные данные - это последовательность идентификаторов курсов, 
+отформатированных следующим образом:
+
+название-ггмм
+Возвращаемое значение функции должно быть отсортировано сначала по ггмм, 
+а затем по названию (которое может иметь разную длину).
+*/
