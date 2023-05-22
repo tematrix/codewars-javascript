e09189d52e72003ef41a934a8de5e97327fa8bf9
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-22 22:19:53 +0300

    Character with longest... - description

---
+++ 6 kyo/character_with_longest_consecutive_repetition.js
@@ -0,0 +1,25 @@
+/*
+DESCRIPTION:
+For a given string s find the character c (or C) with longest consecutive repetition and return:
+
+[c, l]
+
+where l (or L) is the length of the repetition. 
+If there are two or more characters with the same l return the first in order of appearance.
+
+For empty string return:
+["", 0]
+
+
+ПЕРЕВОД:
+Для данной строки s найдите символ c (или C) с наибольшим количеством 
+последовательных повторений и верните:
+
+[c, l]
+
+где l (или L) - длина повторения.
+Если есть два или более символа с одинаковой l, верните первый символ в порядке появления.
+
+Для пустой строки верните:
+["", 0]
+*/
