3bdb582c1761bf7d88fd689e78f3300fa5c4cdcb
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-23 21:48:08 +0300

    Sort array by string length (ver.2) - description

---
+++ 7 kyo/sort_array_by_string_length_2.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION:
+Write a function that takes an array of strings as an argument and returns 
+a sorted array containing the same strings, ordered from shortest to longest.
+
+For example, if this array were passed as an argument:
+
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+Your function would return the following array:
+
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+All of the strings in the array passed to your function will be different lengths, 
+so you will not have to decide how to order multiple strings of the same length.
+
+
+ПЕРЕВОД:
+Напишите функцию, которая принимает массив строк в качестве аргумента и возвращает отсортированный массив, 
+содержащий те же строки, упорядоченные от самой короткой до самой длинной.
+
+Например, если этот массив был передан в качестве аргумента:
+
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+Ваша функция вернет следующий массив:
+
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+Все строки в массиве, переданном в вашу функцию, будут разной длины, 
+поэтому вам не нужно будет решать, как упорядочить несколько строк одинаковой длины.
+*/
