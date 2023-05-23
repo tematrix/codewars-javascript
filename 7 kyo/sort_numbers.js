577cee162d05275742ee8d2e61d4f9434edba7dd
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-23 21:00:45 +0300

    Sort numbers - description

---
+++ 7 kyo/sort_numbers.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+Finish the solution so that it sorts the passed in array of numbers. 
+If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+
+
+ПЕРЕВОД:
+Завершите решение таким образом, чтобы оно сортировало переданный массив чисел. 
+Если функции передается пустой массив или значение null/nil, она должна возвращать пустой массив.
+
+Например:
+
+solution([1, 2, 10, 50, 5]); // должно вернуть [1,2,5,10,50]
+solution(null); // должно вернуть []
+*/
