6e0bb28d69fc21ce46b4dfffc56cb149836362fe
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-25 12:56:19 +0300

    Reverse every other word...- description

---
+++ 6 kyo/reverse_every_other_word_in_the_string.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+Reverse every other word in a given string, then return the string. 
+Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. 
+Punctuation marks should be treated as if they are a part of the word in this kata.
+
+
+ПЕРЕВОД:
+Развернуть каждое второе слово в заданной строке и вернуть полученную строку. 
+Удалить все начальные и конечные пробелы, обеспечив при этом наличие ровно одного 
+пробела между каждым словом. Знаки препинания должны рассматриваться как часть слова в этой задаче.
+
+Пример:
+"hello world" -> "hello dlrow"
+"hello to the world" -> "hello ot eht dlrow"
+*/
