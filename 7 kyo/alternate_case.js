c432413470a21929c0d5b3af8bcddcd3ca53fd45
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-25 13:02:11 +0300

    Alternate case - description

---
+++ 7 kyo/alternate_case.js
@@ -0,0 +1,11 @@
+/*
+DESCRIPTION:
+Write function alternateCase which switch every letter in string from upper to 
+lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+
+
+ПЕРЕВОД:
+Напишите функцию alternateCase, которая меняет регистр каждой буквы в строке: 
+если буква была в верхнем регистре, она становится в нижнем, а если была 
+в нижнем - в верхнем. Например, строка "Hello World" превращается в "hELLO wORLD".
+*/
