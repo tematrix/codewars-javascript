cd17e9bb859eb528cd559b27b04c970e09b828f2
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-27 13:26:35 +0300

    Sum strings as number - description

---
+++ 4 kyo/sum_strings_as_numbers.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+Given the string representations of two integers, return the string representation of the sum of those integers.
+
+For example:
+
+sumStrings('1','2') // => '3'
+A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+
+I have removed the use of BigInteger and BigDecimal in java
+
+Python: your solution need to work with huge numbers (about a milion digits), converting to int will not work.
+
+
+ПЕРЕВОД:
+Даны две строковые записи целых чисел. Вернуть строковую запись суммы этих чисел.
+
+Например:
+
+sumStrings('1','2') // => '3'
+
+Строковая запись целого числа не содержит символов, кроме десяти цифр "0"-"9".
+
+В Java нельзя использовать BigInteger и BigDecimal.
+
+В Python ваше решение должно работать с огромными числами (около миллиона цифр), 
+преобразование в int не будет работать.
+*/
