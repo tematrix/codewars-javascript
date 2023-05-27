47a3ec64cf7220fbbba017049a0a845e3d80d1ea
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-27 13:21:17 +0300

    Total area covered by more rectan... - description

---
+++ 2 kyo/total_area_covered_by_more_rectangles.js
@@ -0,0 +1,33 @@
+/*
+DESCRIPTION:
+Write a function calculate that, given a list of rectangles, finds the area of their union.
+
+Each rectangle is represented by four integers [x0, y0, x1, y1], where (x0, y0) 
+is the lower-left corner and (x1, y1) is the upper-right corner. 
+The origin (0, 0) is bottom left. 
+For the representation used for your language, see the example tests.
+
+See linked kata for a more detailed example.
+
+Constraints
+Performance requirement: Number of rectangles n ≥ 0:
+For JavaScript: 10 tests with n ≤ 5000, and 10 tests with n ≤ 20,000
+For Python: 10 tests with n ≤ 1500, and 7 tests with n ≤ 8000
+Coordinates: 0 ≤ xi, yi ≤ 1,000,000
+
+
+ПЕРЕВОД:
+Напишите функцию calculate, которая по заданному списку прямоугольников находит площадь их объединения.
+
+Каждый прямоугольник представлен четырьмя целыми числами [x0, y0, x1, y1], где (x0, y0) -
+нижний левый угол, а (x1, y1) - верхний правый угол.
+Начало координат (0, 0) находится в нижнем левом углу.
+Для представления используйте примеры тестов для вашего языка программирования.
+
+Смотрите связанный ката для более подробного примера.
+
+Ограничения
+Требования к производительности: количество прямоугольников n ≥ 0:
+Для JavaScript: 10 тестов с n ≤ 5000 и 10 тестов с n ≤ 20000
+Для Python: 10 тестов с n ≤ 1500 и 7 тестов с n ≤ 8000
+*/
