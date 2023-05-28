dc9a14193a4f692e71f0f6ba5148aa05d847f986
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-28 12:31:04 +0300

    Sort arrays (ignoring case) - description

---
+++ 6 kyo/sort_arrays_(ignoring_case).js
@@ -0,0 +1,14 @@
+/*
+DESCRIPTION:
+Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+
+ПЕРЕВОД:
+Отсортируйте данный массив строк в алфавитном порядке без учета регистра. Например:
+
+["Hello", "there", "I'm", "fine"] --> ["fine", "Hello", "I'm", "there"]
+["C", "d", "a", "B"]) --> ["a", "B", "C", "d"]
+*/
