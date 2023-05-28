75f58c0446cf80bf33201c357f25506aab2dc741
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-28 12:32:57 +0300

    Sort arrays (ignoring case) - solution

--- 6 kyo/sort_arrays_(ignoring_case).js
+++ 6 kyo/sort_arrays_(ignoring_case).js
@@ -12,3 +12,8 @@ Sort the given array of strings in alphabetical order, case insensitive. For exa
 ["Hello", "there", "I'm", "fine"] --> ["fine", "Hello", "I'm", "there"]
 ["C", "d", "a", "B"]) --> ["a", "B", "C", "d"]
 */
+
+const sortme = (n) => {return n.sort((a,b) => a.toLowerCase().localeCompare(b.toLowerCase()));}
+
+console.log(sortme(["Hello", "there", "I'm", "fine"]));
+console.log(sortme(["C", "d", "a", "B"]));
