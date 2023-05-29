a9e154b65f439720fa08abbf93c239acb88a7772
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-29 20:39:20 +0300

    Javascript array filter - description

---
+++ 7 kyo/javascript_array_filter.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). 
+Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+
+ПЕРЕВОД:
+В JavaScript для массивов доступна функция filter (начиная с JavaScript 1.6).
+Используйте функциональность filter для выполнения данной функции.
+
+Решение будет работать следующим образом:
+
+getEvenNumbers([2,4,5,6]) // должно быть равно [2,4,6]
+*/
