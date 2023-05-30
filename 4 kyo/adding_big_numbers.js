defede82ab9c963722b4780727bea559e6202ebe
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-30 21:48:02 +0300

    Adding big numbers - description

---
+++ 4 kyo/adding_big_numbers.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION:
+We need to sum big numbers and we require your help.
+
+Write a function that returns the sum of two numbers. 
+The input numbers are strings and the function must return a string.
+
+Example
+add("123", "321"); -> "444"
+add("11", "99");   -> "110"
+
+Notes
+The input numbers are big.
+The input is a string of only digits
+The numbers are positives
+
+
+ПЕРЕВОД:
+Нам нужна помощь в сложении больших чисел.
+
+Напишите функцию, которая возвращает сумму двух чисел. 
+Входные числа представлены строками, а функция должна возвращать строку.
+
+Пример:
+add("123", "321"); -> "444"
+add("11", "99"); -> "110"
+
+Примечания:
+Входные числа большие.
+Вход - строка, содержащая только цифры.
+Числа положительные.
+*/
