6089577bee15167a6a5df5570cab250e6ebc8adc
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-30 21:50:54 +0300

    Adding big numbers - solution

--- 4 kyo/adding_big_numbers.js
+++ 4 kyo/adding_big_numbers.js
@@ -30,3 +30,26 @@ add("11", "99"); -> "110"
 Вход - строка, содержащая только цифры.
 Числа положительные.
 */
+
+function add(a, b) {
+    let result = '', over = 0, d = (x, l) => l >= 0 ? parseInt(x[l]) : 0;
+    
+    for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0; i--, j--) {
+      const sum = d(a, i) + d(b, j) + over;
+      
+      if (sum > 9) {
+        result = (sum - 10) + result;
+        over = 1;
+      } else {
+        result = sum + result;
+        over = 0;
+      }
+    }
+    
+    if (over > 0) {result = over + result;}
+    
+    return result;
+}
+
+console.log(add('123', '321'));
+console.log(add('11','99'));
