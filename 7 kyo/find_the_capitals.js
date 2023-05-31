3f83facb02aadac884494dc805e20428267463ed
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-05-31 13:54:53 +0300

    Find the capitals - description

---
+++ 7 kyo/find_the_capitals.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+Write a function that takes a single string (word) as argument. 
+The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example
+capitals('CodEWaRs'), [0,3,4,6];
+
+
+ПЕРЕВОД:
+Напишите функцию, которая принимает в качестве аргумента одну строку (слово).
+Функция должна возвращать упорядоченный список, содержащий индексы всех заглавных букв в строке.
+
+Пример:
+capitals('CodEWaRs') должна вернуть [0,3,4,6].
+*/
