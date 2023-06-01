101c0c7e9ab24ebb38365a4a6912fb06917eefd3
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-01 11:24:49 +0300

    Largest 5 digit number in a series - description

---
+++ 7 kyo/largest_5_digit_number_in_a_series.js
@@ -0,0 +1,33 @@
+/*
+DESCRIPTION:
+In the following 6 digit number:
+
+283910
+91 is the greatest sequence of 2 consecutive digits.
+
+In the following 10 digit number:
+
+1234567890
+67890 is the greatest sequence of 5 consecutive digits.
+
+Complete the solution so that it returns the greatest sequence of five consecutive digits 
+found within the number given. The number will be passed in as a string of only digits. 
+It should return a five digit integer. The number passed may be as large as 1000 digits.
+
+
+ПЕРЕВОД:
+В следующем шестизначном числе:
+
+283910
+91 - это наибольшая последовательность из 2 последовательных цифр.
+
+В следующем десятизначном числе:
+
+1234567890
+67890 - это наибольшая последовательность из 5 последовательных цифр.
+
+Дополните решение так, чтобы оно возвращало наибольшую последовательность из пяти 
+последовательных цифр, найденных в данном числе. Число передается в виде строки, 
+содержащей только цифры. Функция должна возвращать пятизначное целое число. 
+Число, переданное в функцию, может быть длиной до 1000 цифр.
+*/
