6dcbc384157af6ddb6df7f35fad868502daa80aa
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-02 20:45:56 +0300

    String average - solution

--- 6 kyo/string_average.js
+++ 6 kyo/string_average.js
@@ -18,3 +18,23 @@ If the string is empty or includes a number greater than 9, return "n/a"
 
 Если строка пуста или содержит число больше 9, верните "n/a".
 */
+
+function averageString(str) {
+    const result = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+          s = str.split(' '),
+          num = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 
+                 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9};
+    
+    let sum = 0;
+    for (let e of s) {
+      if (num.hasOwnProperty(e)) {
+        sum += num[e];
+      } else {
+        return 'n/a';
+      }
+    }
+    
+    return result[Math.floor(sum/s.length)];
+}
+
+console.log(averageString("zero nine five two"));
