5b15c6c79484666620ac5d0c5ea62e280d6d9fec
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-02 20:43:02 +0300

    String average - description

---
+++ 6 kyo/string_average.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+You are given a string of numbers between 0-9. 
+Find the average of these numbers and return it as a floored whole 
+number (ie: no decimal places) written out as a string. Eg:
+
+"zero nine five two" -> "four"
+
+If the string is empty or includes a number greater than 9, return "n/a"
+
+
+ПЕРЕВОД:
+Вам дана строка чисел от 0 до 9.
+Найдите среднее значение этих чисел и верните его как округленное 
+целое число (без десятичных знаков), записанное строкой. Например:
+
+"zero nine five two" -> "four"
+
+Если строка пуста или содержит число больше 9, верните "n/a".
+*/
