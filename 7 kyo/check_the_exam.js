30e62e04da125930846a092da47a4df0eb4233cd
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-03 23:27:24 +0300

    Check the exam - solution

--- 7 kyo/check_the_exam.js
+++ 7 kyo/check_the_exam.js
@@ -36,3 +36,22 @@ checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""]) → 7
 checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
 checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"]) → 0
 */
+
+function checkExam(array1, array2) {
+    let result = 0;
+    for (let i = 0; i < array1.length; i++) {    
+      if (array1[i] === array2[i]) {
+        result += 4;
+      } else if (array2[i] === "") {
+        continue;
+      } else {
+        result--;
+      }
+    }
+    return result > 0 ? result : 0;
+}
+
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]));
+console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""]));
+console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]));
+console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"]));
