c7c34eea9a7b9df28931f030d004a55dba86a4c0
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-03 23:17:07 +0300

    Is this a triangle - description

---
+++ 7 kyo/is_this_a_triangle.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+Implement a function that accepts 3 integer values a, b, c. 
+The function should return true if a triangle can be built with 
+the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+
+ПЕРЕВОД:
+Реализуйте функцию, которая принимает 3 целых числа a, b, c.
+Функция должна возвращать true, если можно построить треугольник с длинами сторон,
+равными заданным значениям, и false в любом другом случае.
+
+(В этом случае все треугольники должны иметь площадь больше 0, чтобы быть принятыми).
+*/
