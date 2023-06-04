f0cfcaebae90f66ca5b0f45b85c414dddfa29058
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-04 23:18:52 +0300

    Predict your age - description

---
+++ 7 kyo/predict_your_age.js
@@ -0,0 +1,40 @@
+/*
+DESCRIPTION:
+My grandfather always predicted how old people would get, 
+and right before he passed away he revealed his secret!
+
+In honor of my grandfather's memory we will write a function using his formula!
+
+Take a list of ages when each of your great-grandparent died.
+Multiply each number by itself.
+Add them all together.
+Take the square root of the result.
+Divide by two.
+Example
+predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+Note: the result should be rounded down to the nearest integer.
+
+Some random tests might fail due to a bug in the JavaScript implementation. 
+Simply resubmit if that happens to you.
+
+
+ПЕРЕВОД:
+Мой дед всегда предсказывал, как долго будут жить люди, 
+и прямо перед тем, как умереть, он раскрыл свой секрет!
+
+В честь памяти моего деда мы напишем функцию, используя его формулу!
+
+Возьмите список возрастов, когда умер каждый из ваших прабабушек и прадедушек.
+Умножьте каждое число само на себя.
+Сложите все результаты.
+Возьмите квадратный корень из суммы.
+Разделите результат на два.
+
+Пример:
+predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+
+Примечание: результат должен быть округлен в меньшую сторону до ближайшего целого числа.
+
+Некоторые случайные тесты могут не пройти из-за ошибки в реализации на JavaScript. 
+Просто отправьте решение еще раз, если это произойдет.
+*/
