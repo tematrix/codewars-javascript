be171d639a8930caa178c4cc01f2eb929853e52d
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-04 23:19:51 +0300

    Predict your age - solution

--- 7 kyo/predict_your_age.js
+++ 7 kyo/predict_your_age.js
@@ -38,3 +38,9 @@ predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
 Некоторые случайные тесты могут не пройти из-за ошибки в реализации на JavaScript. 
 Просто отправьте решение еще раз, если это произойдет.
 */
+
+function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+    return Math.floor(Math.sqrt(Array.from(arguments).map(e=>e*e).reduce((s,e)=>s+e,0))/2);
+}
+
+console.log(predictAge(65, 60, 75, 55, 60, 63, 64, 45));
