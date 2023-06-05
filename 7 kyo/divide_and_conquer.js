6a9c30a4d28a1c8216e631756fd3deb044c035b6
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-06 01:14:52 +0300

    Divide and conquer - description

---
+++ 7 kyo/divide_and_conquer.js
@@ -0,0 +1,13 @@
+/*
+DESCRIPTION:
+Given a mixed array of number and string representations of integers, 
+add up the non-string integers and subtract the total of the string integers.
+
+Return as a number.
+
+
+ПЕРЕВОД:
+Дан массив, содержащий числа и строковые представления целых чисел. 
+Необходимо сложить все целочисленные элементы массива и вычесть из этой суммы 
+сумму всех строковых представлений целых чисел. Результатом должно быть число.
+*/
