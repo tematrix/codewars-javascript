35ac310c66817d7db5fcbef86a78bf44c8c0377d
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-05 12:54:25 +0300

    Switcheroo - description

---
+++ 7 kyo/switcheroo.js
@@ -0,0 +1,21 @@
+/*
+DESCRIPTION:
+Given a string made up of letters a, b, and/or c, switch the position 
+of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+
+
+ПЕРЕВОД:
+Дана строка, состоящая из букв a, b и/или c. 
+Поменяйте местами буквы a и b (замените a на b и наоборот). 
+Оставьте все вхождения c нетронутыми.
+
+Пример:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
