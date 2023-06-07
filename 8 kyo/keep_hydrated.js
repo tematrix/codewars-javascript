10181c3547c9f988c58c4048b33fc882b13f9d07
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-07 11:27:42 +0300

    Keep hydrated - description

---
+++ 8 kyo/keep_hydrated.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION:
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, 
+he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return 
+the number of litres Nathan will drink, rounded to the smallest value.
+
+For example:
+
+time = 3 ----> litres = 1
+time = 6.7---> litres = 3
+time = 11.8--> litres = 5
+
+
+ПЕРЕВОД:
+Нейтан любит кататься на велосипеде.
+
+Поскольку Нейтан знает, что важно оставаться гидрированным, 
+он пьет 0,5 литра воды за каждый час катания на велосипеде.
+
+Вам дается время в часах, и вам нужно вернуть количество литров воды, 
+которое Нейтан выпьет, округленное до наименьшего значения.
+
+Например:
+
+time = 3 ----> litres = 1
+time = 6.7---> litres = 3
+time = 11.8--> litres = 5
+*/
