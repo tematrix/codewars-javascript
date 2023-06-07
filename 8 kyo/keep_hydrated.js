cb6065ba3987c0b9a0004d4283efaec0ec2598b8
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-07 11:29:23 +0300

    Keep hydrated - solution

--- 8 kyo/keep_hydrated.js
+++ 8 kyo/keep_hydrated.js
@@ -30,3 +30,11 @@ time = 3 ----> litres = 1
 time = 6.7---> litres = 3
 time = 11.8--> litres = 5
 */
+
+function litres(time) {
+    return Math.floor(time * 0.5);
+}
+
+console.log(litres(3));
+console.log(litres(6.7));
+console.log(litres(11.8));
