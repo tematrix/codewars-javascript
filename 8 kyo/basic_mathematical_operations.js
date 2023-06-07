c2f594438764eb8fa6f3cd1199153a54771dcaf2
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-07 11:34:32 +0300

    Basic mathematical operations - description

---
+++ 8 kyo/basic_mathematical_operations.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+
+
+ПЕРЕВОД:
+Ваша задача - создать функцию, которая выполняет четыре основные математические операции.
+
+Функция должна принимать три аргумента - операцию (строка / символ), значение1 (число) и значение2 (число).
+Функция должна возвращать результат чисел после применения выбранной операции.
+
+Примеры (оператор, значение1, значение2) -> вывод
+('+', 4, 7) -> 11
+('-', 15, 18) -> -3
+('*', 5, 5) -> 25
+('/', 49, 7) -> 7
+*/
