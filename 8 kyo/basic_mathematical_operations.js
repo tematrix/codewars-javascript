dc3464931b00dc9df6ea48a52551b8eed3c9b442
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-07 11:36:57 +0300

    Basic mathematical operations - solution

--- 8 kyo/basic_mathematical_operations.js
+++ 8 kyo/basic_mathematical_operations.js
@@ -24,3 +24,18 @@ Examples(Operator, value1, value2) --> output
 ('*', 5, 5) -> 25
 ('/', 49, 7) -> 7
 */
+
+function basicOp(operation, value1, value2) {
+    switch(operation) {
+        case '+': return value1 + value2;
+        case '-': return value1 - value2;
+        case '*': return value1 * value2;
+        case '/': return value2 !== 0 ? value1 / value2 : 'error';
+        default: return 'error';
+    }
+}
+
+console.log(basicOp('+', 4, 7));
+console.log(basicOp('-', 15, 18));
+console.log(basicOp('*', 5, 5));
+console.log(basicOp('/', 49, 7));
