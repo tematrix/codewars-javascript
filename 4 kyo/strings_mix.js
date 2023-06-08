6f962a775bbb7eb764aecd3b5dc8d933677dfadd
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-08 12:07:22 +0300

    Strings mix - solution

--- 4 kyo/strings_mix.js
+++ 4 kyo/strings_mix.js
@@ -105,3 +105,69 @@ s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
 s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
 mix(s1, s2) --> "1:mmmmmm/E:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/E:ee/E:ss"
 */
+
+function mix(s1,s2) {
+    const countLetters = (s) => {
+      const counts = {},
+            regex = /[a-z]/g,
+            letters = s.match(regex);
+      for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        counts[letter] = counts[letter] ? counts[letter] + 1 : 1;
+      }
+      return counts;
+    };
+    
+    const counts1 = countLetters(s1),
+          counts2 = countLetters(s2),
+          maxStrings = [];
+    
+    const addLetters = (a, b) => {
+      for (let letter of Object.keys(b)) {
+        if (a[letter]) {
+          continue;
+        } else {
+          a[letter] = 1;
+        }
+      }
+    }
+    
+    addLetters(counts1, counts2);
+    addLetters(counts2, counts1);
+  
+    for (let letter of Object.keys(counts1)) {
+      if (counts1[letter] <= 1 && counts2[letter] <= 1) {
+        continue;
+      }
+      if (counts1[letter] === counts2[letter]) {
+        maxStrings.push(`3:${letter.repeat(counts1[letter])}`);
+      } else if (counts1[letter] > counts2[letter]) {
+        maxStrings.push(`1:${letter.repeat(counts1[letter])}`);
+      } else {
+        maxStrings.push(`2:${letter.repeat(counts2[letter])}`);
+      }
+    }
+    
+    maxStrings.sort((a,b) => {
+      return a.length !== b.length ? b.length - a.length : a.localeCompare(b);
+    });
+    
+    return maxStrings.filter(e => e.length > 2).join('/').replace(/3/g,'=');
+}
+
+let s1 = "my&friend&Paul has heavy hats! &";
+let s2 = "my friend John has many many friends &";
+console.log(mix(s1, s2));
+
+s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &";
+s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&";
+console.log(mix(s1, s2));
+
+s1 = "Are the kids at home? aaaaa fffff";
+s2 = "Yes they are here! aaaaa fffff";
+console.log(mix(s1, s2));
+
+
+s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &";
+s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&";
+console.log(mix(s1, s2));
