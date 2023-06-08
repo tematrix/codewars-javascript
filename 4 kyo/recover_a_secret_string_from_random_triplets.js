5c87b75d6be344c44bed97c5ba89b7cbff22e6f7
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-08 12:10:17 +0300

    Recover a secret string - description

---
+++ 4 kyo/recover_a_secret_string_from_random_triplets.js
@@ -0,0 +1,33 @@
+/*
+DESCRIPTION:
+There is a secret string which is unknown to you. 
+Given a collection of random triplets from the string, recover the original string.
+
+A triplet here is defined as a sequence of three letters such that each letter 
+occurs somewhere before the next in the given string. 
+"whi" is a triplet for the string "whatisup".
+
+As a simplification, you may assume that no letter occurs more than once in the secret string.
+
+You can assume nothing about the triplets given to you other than that they are valid 
+triplets and that they contain sufficient information to deduce the original string. 
+In particular, this means that the secret string will never contain letters that 
+do not occur in one of the triplets given to you.
+
+
+ПЕРЕВОД:
+Есть секретная строка, которая вам неизвестна.
+У вас есть коллекция случайных троек из этой строки, и нужно восстановить исходную строку.
+
+Тройка здесь определяется как последовательность из трех букв, таких что каждая буква
+встречается где-то перед следующей в данной строке.
+"whi" является тройкой для строки "whatisup".
+
+Для упрощения задачи можно предположить, что ни одна буква 
+не встречается более одного раза в секретной строке.
+
+Вы ничего не можете предположить о данных тройках, кроме того, что они являются действительными
+тройками и содержат достаточно информации, чтобы вывести исходную строку.
+В частности, это означает, что секретная строка никогда не будет содержать букв,
+которые не встречаются в одной из предоставленных вам троек.
+*/
