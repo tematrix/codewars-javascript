3fb5a226b11361816a12cbf2b7a438fdbc476bea
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-09 17:18:33 +0300

    Number of people in the bus (ver2) - solution

--- 7 kyo/number_of_people_in_the_bus_2.js
+++ 7 kyo/number_of_people_in_the_bus_2.js
@@ -36,3 +36,7 @@ The second value in the first pair in the array is 0, since the bus is empty in 
 
 Второе значение в первой паре в массиве равно 0, так как автобус пуст на первой автобусной остановке.
 */
+
+var number = (busStops) => busStops.reduce((p,c) => p + c[0] - c[1], 0);
+
+console.log(number([[15, 5], [20, 4], [8, 10]]));
