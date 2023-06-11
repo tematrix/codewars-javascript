babadebf88da259031e37938b84b02a94621b97a
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-11 09:00:37 +0300

    Sum of a sequence - solution

--- 7 kyo/sum_of_a_sequence.js
+++ 7 kyo/sum_of_a_sequence.js
@@ -32,3 +32,15 @@ Examples
 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
 1,5,3 --> 5 (1 + 4)
 */
+
+const sequenceSum = (begin, end, step) => {
+    if (begin > end) {return 0;}
+    let sum = 0;
+    for (let i = begin; i <= end; i += step) {sum += i;}
+    return sum;
+};
+
+console.log(sequenceSum(2, 2, 2));
+console.log(sequenceSum(2, 6, 2));
+console.log(sequenceSum(1, 5, 1));
+console.log(sequenceSum(1, 5, 3));
