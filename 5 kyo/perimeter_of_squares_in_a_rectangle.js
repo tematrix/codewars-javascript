075d26aabb8c04f07be158e426a6f929606a2a31
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-13 10:45:45 +0300

    Perimeter of squares in a rectangle - description

---
+++ 5 kyo/perimeter_of_squares_in_a_rectangle.js
@@ -0,0 +1,40 @@
+/*
+DESCRIPTION:
+The drawing shows 6 squares the sides of which have a length of 1, 1, 2, 3, 5, 8. 
+It's easy to see that the sum of the perimeters of these squares is : 
+4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80 
+
+Could you give the sum of the perimeters of all the squares in a rectangle 
+when there are n + 1 squares disposed in the same manner as in the drawing:
+
+alternative text
+
+Hint:
+See Fibonacci sequence
+
+The function perimeter has for parameter n where n + 1 is the number of squares 
+(they are numbered from 0 to n) and returns the total perimeter of all the squares.
+
+perimeter(5)  should return 80
+perimeter(7)  should return 216
+
+
+ПЕРЕВОД:
+На рисунке изображены 6 квадратов, стороны которых имеют длину 1, 1, 2, 3, 5, 8. 
+Легко видеть, что сумма периметров этих квадратов равна : 
+4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80 
+
+Назовите сумму периметров всех квадратов прямоугольника 
+если в нем n + 1 квадратов, расположенных так же, как на рисунке:
+
+альтернативный текст
+
+Подсказка:
+См. последовательность Фибоначчи
+
+Функция периметра имеет параметр n, где n + 1 - количество квадратов. 
+(они нумеруются от 0 до n) и возвращает общий периметр всех квадратов.
+
+периметр(5) должен возвращать 80
+периметр(7) должен возвращать 216
+*/
