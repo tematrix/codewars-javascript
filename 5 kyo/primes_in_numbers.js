6a410d23c2b74beb4dffdbec8e7cda4c1f445279
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-13 10:49:44 +0300

    Primes in numbers - description

---
+++ 5 kyo/primes_in_numbers.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+Given a positive number n > 1 find the prime factor decomposition of n. 
+The result will be a string with the following form :
+
+"(p1**n1)(p2**n2)...(pk**nk)"
+with the p(i) in increasing order and n(i) empty if n(i) is 1.
+
+Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
+
+
+ПЕРЕВОД:
+Для положительного числа n > 1 найдите разложение n по простым факторам. 
+Результатом будет строка следующего вида :
+
+"(p1**n1)(p2**n2)...(pk**nk)"
+с p(i) в порядке возрастания и n(i) пустой, если n(i) равно 1.
+
+Пример: n = 86240 должен вернуть "(2**5)(5)(7**2)(11)".
+*/
