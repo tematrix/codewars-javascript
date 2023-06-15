d992a6130d3c58920a48acfe7fa6363d18e8566e
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-15 10:43:13 +0300

    Count ip addresses - description

---
+++ 5 kyo/count_ip_addresses.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+Implement a function that receives two IPv4 addresses, and returns the number 
+of addresses between them (including the first one, excluding the last one).
+
+All inputs will be valid IPv4 addresses in the form of strings. 
+The last address will always be greater than the first one.
+
+Examples
+* With input "10.0.0.0", "10.0.0.50"  => return   50 
+* With input "10.0.0.0", "10.0.1.0"   => return  256 
+* With input "20.0.0.10", "20.0.1.0"  => return  246
+
+
+ПЕРЕВОД:
+Реализуйте функцию, которая получает два адреса IPv4 и возвращает количество 
+адресов между ними (включая первый и исключая последний).
+
+Все входные данные будут действительными адресами IPv4 в виде строк. 
+Последний адрес всегда будет больше первого.
+
+Примеры
+* С входными данными "10.0.0.0", "10.0.0.50" => return 50 
+* При вводе "10.0.0.0", "10.0.1.0" => return 256 
+* С вводом "20.0.0.10", "20.0.1.0" => return 246
+*/
