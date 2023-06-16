c14f5958b32b20f73b63618c840ee79b69333e92
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-16 10:05:54 +0300

    Max diff easy - description

---
+++ 7 kyo/max_diff_easy.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+You must implement a function that returns the difference between the largest 
+and the smallest value in a given list / array (lst) received as the parameter.
+
+lst contains integers, that means it may contain some negative numbers
+if lst is empty or contains a single element, return 0
+lst is not sorted
+[1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+[1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+
+
+ПЕРЕВОД:
+Вы должны реализовать функцию, которая возвращает разницу между наибольшим 
+и наименьшим значением в заданном списке/массиве (lst), полученном в качестве параметра.
+
+lst содержит целые числа, то есть может содержать некоторые отрицательные числа
+если lst пуст или содержит один элемент, возвращается 0
+lst не отсортирован
+[1, 2, 3, 4] // возвращает 3, потому что 4 - 1 == 3
+[1, 2, 3, -4] // возвращает 7, потому что 3 - (-4) == 7
+*/
