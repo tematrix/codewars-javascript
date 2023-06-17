e55d5b2080f711441bef52b9b8b931f8b262e68b
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-18 00:44:20 +0300

    Beeramid - description

---
+++ 5 kyo/beeramid.js
@@ -0,0 +1,45 @@
+/*
+DESCRIPTION:
+Let's pretend your company just hired your friend from college and paid you a referral bonus. 
+Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using 
+the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. 
+And then probably drink those beers, because let's pretend it's Friday too.
+
+A beer can pyramid will square the number of cans in each level - 1 can in the top level, 
+4 in the second, 9 in the next, 16, 25...
+
+Complete the beeramid function to return the number of complete levels of a beer can pyramid 
+you can make, given the parameters of:
+
+your referral bonus, and
+
+the price of a beer can
+
+For example:
+
+beeramid(1500, 2); // should === 12
+beeramid(5000, 3); // should === 16
+
+
+ПЕРЕВОД:
+Давайте представим, что ваша компания только что приняла на работу вашего друга из колледжа 
+и выплатила вам реферальный бонус. Потрясающе! Чтобы отпраздновать это событие, вы приглашаете 
+свою команду в ужасный бар по соседству и используете реферальный бонус, чтобы купить и построить 
+самую большую трехмерную пирамиду из пивных банок, какую только можно. 
+А потом, вероятно, выпьете это пиво, потому что давайте представим, что сегодня тоже пятница.
+
+В пирамиде из пивных банок количество банок на каждом уровне будет квадратным - 1 банка на верхнем уровне, 
+4 во втором, 9 в следующем, 16, 25...
+
+Дополните функцию beeramid, чтобы вернуть количество полных уровней пирамиды из пивных банок 
+которые вы можете сделать, учитывая параметры:
+
+ваш реферальный бонус, и
+
+цена банки пива
+
+Например:
+
+beeramid(1500, 2); // должно === 12
+beeramid(5000, 3); // должно === 16
+*/
