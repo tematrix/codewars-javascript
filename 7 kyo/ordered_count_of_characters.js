6f62f8d7cb8348a25aa7ad984aa87a3e74049ae5
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-17 09:16:51 +0300

    Ordered count of characters - description

---
+++ 7 kyo/ordered_count_of_characters.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+Count the number of occurrences of each character and return it 
+as a (list of tuples) in order of appearance. For empty output return (an empty list).
+
+Consult the solution set-up for the exact data structure implementation depending on your language.
+
+Example:
+orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+
+
+ПЕРЕВОД:
+Подсчитайте количество вхождений каждого символа и верните его 
+в виде (списка кортежей) в порядке появления. Для пустого вывода возвращается (пустой список).
+
+Точную реализацию структуры данных в зависимости от языка можно узнать из комплекта решения.
+
+Пример:
+orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]].
+*/
