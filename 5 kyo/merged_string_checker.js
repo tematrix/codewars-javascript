ade5719daf9dae0ec4e1e6b3edf8ebc750026632
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-19 22:03:12 +0300

    Merged string checker - description

---
+++ 5 kyo/merged_string_checker.js
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION:
+At a job interview, you are challenged to write an algorithm to check if a given string, s, 
+can be formed from two other strings, part1 and part2.
+
+The restriction is that the characters in part1 and part2 should be in the same order as in s.
+
+The interviewer gives you the following example and tells you to figure out the rest from the given test cases.
+
+For example:
+
+'codewars' is a merge from 'cdw' and 'oears':
+
+    s:  c o d e w a r s   = codewars
+part1:  c   d   w         = cdw
+part2:    o   e   a r s   = oears
+
+
+ПЕРЕВОД:
+На собеседовании вас попросили написать алгоритм для проверки того, может ли данная строка, s, 
+может быть сформирована из двух других строк, part1 и part2.
+
+Ограничение состоит в том, что символы в part1 и part2 должны располагаться в том же порядке, что и в s.
+
+Интервьюер дает вам следующий пример и просит выяснить остальное из приведенных тестовых примеров.
+
+Например:
+
+'codewars' - это объединение 'cdw' и 'oears':
+
+    s: c o d e w a r s = codewars
+часть1: c d w = cdw
+часть2: o e a r s = oears
+*/
