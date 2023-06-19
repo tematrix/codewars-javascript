2a4f06f0969bdea39736b0b4893d6eabb6fc7adf
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-19 21:55:13 +0300

    Convert a hex string to rgb - description

---
+++ 5 kyo/convert_a_hex_string_to_rgb.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+When working with color values it can sometimes be useful to extract the individual red, green, 
+and blue (RGB) component values for a color. Implement a function that meets these requirements:
+
+Accepts a case-insensitive hexadecimal color string as its parameter (ex. "#FF9933" or "#ff9933")
+Returns a Map<String, int> with the structure {r: 255, g: 153, b: 51} where r, g, and b range from 0 through 255
+Note: your implementation does not need to support the shorthand form of hexadecimal notation (ie "#FFF")
+
+Example
+"#FF9933" --> {r: 255, g: 153, b: 51}
+
+
+ПЕРЕВОД:
+При работе с цветовыми значениями иногда бывает полезно извлечь отдельные значения красного, зеленого, 
+и синего (RGB) компонентов для цвета. Реализуйте функцию, отвечающую этим требованиям:
+
+Принимает в качестве параметра шестнадцатеричную строку цвета без учета регистра (например, "#FF9933" или "#ff9933").
+Возвращает Map<String, int> со структурой {r: 255, g: 153, b: 51}, где r, g и b находятся в диапазоне от 0 до 255.
+Примечание: ваша реализация не обязана поддерживать сокращенную форму шестнадцатеричной нотации (т.е. "#FFF").
+
+Пример
+"#FF9933" --> {r: 255, g: 153, b: 51}
+*/
