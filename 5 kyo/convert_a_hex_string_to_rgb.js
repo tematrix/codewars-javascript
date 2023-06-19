4f0b92229965cee2bfa6ee9bb8bbb8cea5bb91bc
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-19 21:57:00 +0300

    Convert a hex string to rgb - solution

--- 5 kyo/convert_a_hex_string_to_rgb.js
+++ 5 kyo/convert_a_hex_string_to_rgb.js
@@ -22,3 +22,17 @@ Example
 Пример
 "#FF9933" --> {r: 255, g: 153, b: 51}
 */
+
+function hexStringToRGB(hexString) {
+    hexString = hexString.replace('#', '');
+    
+    const rgb = {};
+    
+    rgb['r'] = parseInt(hexString.substring(0, 2), 16),
+    rgb['g'] = parseInt(hexString.substring(2, 4), 16),
+    rgb['b'] = parseInt(hexString.substring(4, 6), 16);
+    
+    return rgb;
+}
+
+console.log(hexStringToRGB("#FF9933"));
