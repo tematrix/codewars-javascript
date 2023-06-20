d7e684cce56c48c1edefe19c6c88cb543c58cf59
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-20 23:47:16 +0300

    Land perimeter - description

---
+++ 5 kyo/land_perimeter.js
@@ -0,0 +1,52 @@
+/*
+DESCRIPTION:
+Given an array arr of strings, complete the function by calculating the total perimeter of all the islands. 
+Each piece of land will be marked with 'X' while the water fields are represented as 'O'. 
+Consider each tile being a perfect 1 x 1 piece of land. Some examples for better visualization:
+
+['XOOXO',
+ 'XOOXO',
+ 'OOOXO',
+ 'XXOXO',
+ 'OXOOO'] 
+which represents:
+
+should return: "Total land perimeter: 24".
+
+Following input:
+
+['XOOO',
+ 'XOXO',
+ 'XOXO',
+ 'OOXX',
+ 'OOOO']
+which represents:
+
+should return: "Total land perimeter: 18"
+
+
+ПЕРЕВОД:
+Получив массив arr строк, выполните функцию, вычислив общий периметр всех островов. 
+Каждый участок суши будет обозначен символом "X", а водные поля - символом "O". 
+Считайте, что каждая плитка - это идеальный участок суши размером 1 x 1. Некоторые примеры для лучшей визуализации:
+
+['XOOXO',
+ 'XOOXO',
+ 'OOOXO',
+ 'XXOXO',
+ 'OXOOO'] 
+что представляет:
+
+должно вернуться: "Общий периметр земли: 24".
+
+Следующий ввод:
+
+['XOOO',
+ 'XOXO',
+ 'XOXO',
+ 'OOXX',
+ 'OOOO']
+что представляет:
+
+должно возвращаться: "Общий периметр земли: 18"
+*/
