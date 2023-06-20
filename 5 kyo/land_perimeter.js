27c8b14c583cd246e3f823ee397efe66d727e47c
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-20 23:47:57 +0300

    Land perimeter - solution

--- 5 kyo/land_perimeter.js
+++ 5 kyo/land_perimeter.js
@@ -50,3 +50,25 @@ should return: "Total land perimeter: 18"
 
 должно возвращаться: "Общий периметр земли: 18"
 */
+
+function landPerimeter(arr) {
+    let perimeter = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[0].length; j++) {
+        if (arr[i][j] === 'X') {
+          perimeter += 4;
+          
+          if (i > 0 && arr[i - 1][j] === 'X') {
+            perimeter -= 2;
+          }
+          
+          if (j > 0 && arr[i][j - 1] === 'X') {
+            perimeter -= 2;
+          }
+        }
+      }
+    }
+    
+    return `Total land perimeter: ${perimeter}`;
+}
