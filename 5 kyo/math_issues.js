2a59d02f1d1c854c8c230bdb6def051dd737baaa
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-23 20:55:21 +0300

    Math issues - description

---
+++ 5 kyo/math_issues.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions? 
+We can assure, that only non-negative numbers are passed as arguments. 
+So you don't have to consider things like undefined, null, NaN, negative numbers, strings and so on.
+
+Here is a list of functions, we need:
+
+Math.round()
+Math.ceil()
+Math.floor()
+
+
+ПЕРЕВОД:
+О нет, наш объект Math был "случайно" сброшен. Можете ли вы повторно реализовать некоторые из этих функций? 
+Мы можем гарантировать, что в качестве аргументов передаются только неотрицательные числа. 
+Поэтому вам не придется рассматривать такие вещи, как undefined, null, NaN, отрицательные числа, строки и так далее.
+
+Вот список функций, которые нам нужны:
+
+Math.round()
+Math.ceil()
+Math.floor()
+*/
