4884f3e509e389a111bc01f47c653360d7539612
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-24 23:33:31 +0300

    Longest palindrome - description

---
+++ 6 kyo/longest_palindrome.js
@@ -0,0 +1,31 @@
+/*
+DESCRIPTION:
+Find the length of the longest substring in the given string s that is the same in reverse.
+
+As an example, if the input was “I like racecars that go fast”, the substring (racecar) length would be 7.
+
+If the length of the input string is 0, the return value must be 0.
+
+Example:
+"a" -> 1 
+"aab" -> 2  
+"abcde" -> 1
+"zzbaabcd" -> 4
+"" -> 0
+
+
+ПЕРЕВОД:
+Найдите длину самой длинной подстроки в заданной строке s, которая совпадает с ней в обратном порядке.
+
+В качестве примера, если вводимая строка была "Мне нравятся гоночные автомобили, 
+которые ездят быстро", длина подстроки (гоночный автомобиль) будет равна 7.
+
+Если длина входной строки равна 0, возвращаемое значение должно быть равно 0.
+
+Пример:
+"a" -> 1 
+"aab" -> 2  
+"abcde" -> 1
+"zzbaabcd" -> 4
+"" -> 0
+*/
