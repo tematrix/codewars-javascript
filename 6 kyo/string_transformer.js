c5da677828d984f123fbf852c4bb364b43fb308e
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-25 22:35:45 +0300

    String transformer - description

---
+++ 6 kyo/string_transformer.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+Given a string, return a new string that has transformed based on the input:
+
+Change case of every character, ie. lower case to upper case, upper case to lower case.
+Reverse the order of words from the input.
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+For example:
+
+"Example Input" ==> "iNPUT eXAMPLE"
+You may assume the input only contain English alphabet and spaces.
+
+
+ПЕРЕВОД:
+Получив строку, вернуть новую строку, преобразованную на основе входных данных:
+
+Изменить регистр каждого символа, т.е. нижний регистр на верхний, верхний регистр на нижний.
+Измените порядок слов в исходном тексте.
+Примечание: Вам придется обрабатывать множественные пробелы, а также ведущие и последующие пробелы.
+
+Например:
+
+"Пример ввода" ==> "iNPUT eXAMPLE".
+Вы можете считать, что входные данные содержат только английский алфавит и пробелы.
+*/
