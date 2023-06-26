2fa1733b4d9e901708bb43a1eeba9bec3263cee1
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-26 20:46:12 +0300

    What century is it - solution

--- 6 kyo/what_century_is_it.js
+++ 6 kyo/what_century_is_it.js
@@ -22,3 +22,20 @@ Examples
 "1124" --> "12-й"
 "2000" --> "20-й"
 */
+
+function whatCentury(year) {
+    let century = Math.ceil(parseInt(year) / 100),
+        suffix = '';
+    
+    if (century % 10 === 1 && century !== 11) {
+      suffix = 'st';
+    } else if (century % 10 === 2 && century !== 12) {
+      suffix = 'nd';
+    } else if (century % 10 === 3 && century !== 13) {
+      suffix = 'rd';
+    } else {
+      suffix = 'th';
+    }
+    
+    return century + suffix;
+}
