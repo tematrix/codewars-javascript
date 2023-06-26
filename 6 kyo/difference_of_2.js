5bdfc250f716b09a1d2daf993c93e045a689f4a9
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-26 20:43:20 +0300

    Difference of 2 - solution

--- 6 kyo/difference_of_2.js
+++ 6 kyo/difference_of_2.js
@@ -33,3 +33,19 @@ Examples
 
 [4, 3, 1, 5, 6] следует вернуть [[1, 3], [3, 5], [4, 6]]
 */
+
+function twosDifference(input){
+    input.sort((a,b) => a - b);
+    
+    const pairs = [];
+    
+    for (let i = 0; i < input.length; i++) {
+      for (let j = i + 1; j < input.length; j++) {
+        if (input[j] - input[i] === 2) {
+          pairs.push([input[i], input[j]]);
+        }
+      }
+    }
+    
+    return pairs;
+}
