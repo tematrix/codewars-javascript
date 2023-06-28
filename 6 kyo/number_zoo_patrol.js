f4a908b7b64261d1c5b5b25d9c04a5097fd86d75
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-28 21:17:19 +0300

    Number zoo patrol - description

---
+++ 6 kyo/number_zoo_patrol.js
@@ -0,0 +1,42 @@
+/*
+DESCRIPTION:
+You're working in a number zoo, and it seems that one of the numbers has gone missing!
+
+Zoo workers have no idea what number is missing, and are too incompetent to figure it out, 
+so they're hiring you to do it for them.
+
+In case the zoo loses another number, they want your program 
+to work regardless of how many numbers there are in total.
+
+Task:
+Write a function that takes a shuffled list of unique numbers from 1 to n with 
+one element missing (which can be any number including n). Return this missing number.
+
+Note: huge lists will be tested.
+
+Examples:
+[1, 3, 4]  =>  2
+[1, 2, 3]  =>  4
+[4, 2, 3]  =>  1
+
+
+ПЕРЕВОД:
+Вы работаете в зоопарке цифр, и кажется, что одна из цифр пропала!
+
+Работники зоопарка понятия не имеют, какое число пропало, и слишком некомпетентны, чтобы это выяснить, 
+поэтому они нанимают вас, чтобы вы сделали это за них.
+
+На случай, если зоопарк потеряет еще один номер, они хотят, чтобы ваша программа 
+будет работать независимо от того, сколько всего номеров.
+
+Задача:
+Напишите функцию, которая принимает перетасованный список уникальных чисел от 1 до n с 
+в котором отсутствует один элемент (это может быть любое число, включая n). Верните это недостающее число.
+
+Примечание: будут проверяться огромные списки.
+
+Примеры:
+[1, 3, 4] => 2
+[1, 2, 3] => 4
+[4, 2, 3] => 1
+*/
