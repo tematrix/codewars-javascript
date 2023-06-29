1e36fcaa63a7642d925dc1c01699f05a238400e1
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-29 16:52:52 +0300

    Sum of two integers - description

---
+++ 6 kyo/sum_of_two_integers.js
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION:
+Given Two integers a , b , find The sum of them , BUT You are not allowed to use the operators + and -
+
+Notes
+The numbers (a,b) may be positive , negative values or zeros .
+
+Returning value will be an integer .
+
+Javascript: the Array reduce methods are disabled, along with eval, require, and module .
+Input >> Output Examples
+1 - Add (5,19) ==> return (24)
+2 - Add (-27,18) ==> return (-9)
+3 - Add (-14,-16) ==> return (-30)
+
+
+ПЕРЕВОД:
+Даны два целых числа a, b, найдите их сумму, НО вам не разрешается использовать операторы + и -
+
+Примечания
+Числа (a,b) могут быть положительными, отрицательными или нулями.
+
+Возвращаемое значение будет целым числом.
+
+Javascript: методы уменьшения массива отключены, также как eval, require и module.
+Примеры ввода >> вывода
+1 - Сложить (5,19) ==> возврат (24)
+2 - Добавить (-27,18) ==> возврат (-9)
+3 - Добавить (-14,-16) ==> возврат (-30)
+*/
