443800c59949c638ae3a0c665f193874d77ca7ae
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-29 16:55:25 +0300

    Simple frequency sort - description

---
+++ 6 kyo/simple_frequency_sort.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+In this kata, you will sort elements in an array by decreasing frequency of elements. 
+If two elements have the same frequency, sort them by increasing value.
+
+solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
+-- We sort by highest frequency to lowest frequency.
+-- If two elements have same frequency, we sort by increasing value.
+
+
+ПЕРЕВОД:
+В этом ката вы будете сортировать элементы в массиве по убыванию частоты элементов. 
+Если два элемента имеют одинаковую частоту, отсортируйте их по возрастанию.
+
+solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
+-- Мы сортируем по наибольшей частоте к наименьшей.
+-- Если два элемента имеют одинаковую частоту, мы сортируем по возрастанию значения.
+*/
