b2a112caac55d029dcb465fb49bfd56a648649ff
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-29 16:56:33 +0300

    Simple frequency sort - solution

--- 6 kyo/simple_frequency_sort.js
+++ 6 kyo/simple_frequency_sort.js
@@ -16,3 +16,32 @@ solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
 -- Мы сортируем по наибольшей частоте к наименьшей.
 -- Если два элемента имеют одинаковую частоту, мы сортируем по возрастанию значения.
 */
+
+function solve(arr) {
+    let freq = {};
+    
+    for (let i = 0; i < arr.length; i++) {
+      freq[arr[i]] = (freq[arr[i]] || 0) + 1;
+    }
+    
+    let pairs = Object.entries(freq).sort(compare),
+        result = [];
+    
+    for (let i = 0; i < pairs.length; i++) {
+      let [num, count] = pairs[i];
+      
+      for (let j = 0; j < count; j++) {
+        result.push(parseInt(num));
+      }
+    }
+    
+    return result;
+}
+  
+function compare(a, b) {
+    if (a[1] === b[1]) {
+      return a[0] - b[0];
+    }
+    
+    return b[1] - a[1];
+}
