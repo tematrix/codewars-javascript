867740cae3c903177834da823f5655c348dbf4f2
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-06-29 16:50:30 +0300

    Twisted sum - solution

--- 6 kyo/twisted_sum.js
+++ 6 kyo/twisted_sum.js
@@ -26,3 +26,18 @@ Examples
 # N = 12
 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) + (1 + 1) + (1 + 2) = 51
 */
+
+function twistedSum(n) {
+    let sum = 0;
+    
+    for (let i = 1; i <= n; i++) {
+      let num = i;
+      
+      while (num > 0) {
+        sum += num % 10;
+        num = Math.floor(num / 10);
+      }
+    }
+    
+    return sum;
+}
