2f3865298d5604aeb5a04e9e4865b6032013810a
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-07-02 13:04:54 +0300

    Pair of gloves - description

---
+++ 6 kyo/pair_of_gloves.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION:
+Winter is coming, you must prepare your ski holidays. 
+The objective of this kata is to determine the number of pair of gloves 
+you can constitute from the gloves you have in your drawer.
+
+Given an array describing the color of each glove, return the number of 
+pairs you can constitute, assuming that only gloves of the same color can form pairs.
+
+Examples:
+input = ["red", "green", "red", "blue", "blue"]
+result = 2 (1 red pair + 1 blue pair)
+
+input = ["red", "red", "red", "red", "red", "red"]
+result = 3 (3 red pairs)
+
+
+ПЕРЕВОД:
+Приближается зима, нужно готовиться к лыжному отдыху. 
+Цель этого ката - определить количество пар перчаток. 
+которые вы можете составить из имеющихся у вас в ящике перчаток.
+
+Учитывая массив, описывающий цвет каждой перчатки, верните количество пар, 
+которые вы можете составить, предполагая, что только перчатки одного цвета могут образовывать пары.
+
+Примеры:
+input = ["красный", "зеленый", "красный", "синий", "синий"].
+результат = 2 (1 красная пара + 1 синяя пара)
+
+вход = ["красный", "красный", "красный", "красный", "красный", "красный"].
+результат = 3 (3 красные пары)
+*/
