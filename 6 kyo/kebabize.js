38e9fbc3bea3f2024aafc7f31f34394e06b1afeb
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-07-02 12:56:50 +0300

    Kebabize - description

---
+++ 6 kyo/kebabize.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+"camelsHaveThreeHumps"  -->  "camels-have-three-humps"
+"camelsHave3Humps"  -->  "camels-have-humps"
+"CAMEL"  -->  "c-a-m-e-l"
+
+Notes:
+the returned string should only contain lowercase letters
+
+
+ПЕРЕВОД:
+Измените функцию kebabize так, чтобы она преобразовывала строку в верблюжьем регистре в шашлычный регистр.
+
+"camelsHaveThreeHumps" --> "camels-have-three-humps"
+"camelsHave3Humps" --> "camels-have-humps"
+"CAMEL" --> "c-a-m-e-l".
+
+Примечания:
+возвращаемая строка должна содержать только строчные буквы
+*/
