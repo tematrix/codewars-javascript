7731432ead7399f77c4a833db4f54b0f73f1aeab
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-07-03 23:09:29 +0300

    The deaf rats of hamelin - description

---
+++ 6 kyo/the_deaf_rats_of_hamelin.js
@@ -0,0 +1,38 @@
+/*
+DESCRIPTION:
+The Pied Piper has been enlisted to play his magical tune and coax all the rats out of town.
+
+But some of the rats are deaf and are going the wrong way!
+
+Kata Task
+How many deaf rats are there?
+
+Legend
+P = The Pied Piper
+O~ = Rat going left
+~O = Rat going right
+
+Example
+ex1 ~O~O~O~O P has 0 deaf rats
+ex2 P O~ O~ ~O O~ has 1 deaf rat
+ex3 ~O~O~O~OP~O~OO~ has 2 deaf rats
+
+
+ПЕРЕВОД:
+Крысолов пригласил Крысолова сыграть свою волшебную мелодию и выманить всех крыс из города.
+
+Но некоторые крысы глухие и идут не туда!
+
+Задание Ката
+Сколько всего глухих крыс?
+
+Легенда
+П = Крысолов
+О~ = Крыса идет налево
+~O = Крыса, идущая направо
+
+Пример
+ex1 ~O~O~O~O~O P имеет 0 глухих крыс
+ex2 P O~ O~ O~ ~O O~ имеет 1 глухую крысу
+ex3 ~O~O~O~O~OP~O~OO~ имеет 2 глухих крысы
+*/
