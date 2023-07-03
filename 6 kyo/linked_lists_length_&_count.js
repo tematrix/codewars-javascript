d48bd9b7924e349d27fc62195ddc4ace2871c79f
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-07-03 23:06:19 +0300

    Linked lists length & count - description

---
+++ 6 kyo/linked_lists_length_&_count.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+Linked Lists - Length & Count
+
+Implement Length() to count the number of nodes in a linked list.
+
+length(null) => 0
+length(1 -> 2 -> 3 -> null) => 3
+Implement Count() to count the occurrences of an integer in a linked list.
+
+count(null, 1) => 0
+count(1 -> 2 -> 3 -> null, 1) => 1
+count(1 -> 1 -> 1 -> 2 -> 2 -> 2 -> 2 -> 3 -> 3 -> null, 2) => 4
+
+
+ПЕРЕВОД:
+Связанные списки - длина и подсчет
+
+Реализуйте функцию Length() для подсчета количества узлов в связанном списке.
+
+length(null) => 0
+length(1 -> 2 -> 3 -> null) => 3
+Реализуйте Count() для подсчета вхождений целого числа в связном списке.
+
+count(null, 1) => 0
+count(1 -> 2 -> 3 -> null, 1) => 1
+count(1 -> 1 -> 1 -> 2 -> 2 -> 2 -> 2 -> 3 -> 3 -> null, 2) => 4
+*/
