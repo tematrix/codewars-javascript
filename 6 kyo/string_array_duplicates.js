c127bdf81d20fe2a328f02cd2bf0e55a7a081351
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-07-03 23:13:14 +0300

    String array duplicates - description

---
+++ 6 kyo/string_array_duplicates.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+In this Kata, you will be given an array of strings and your task is to 
+remove all consecutive duplicate letters from each string in the array.
+
+For example:
+
+dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+dup(["kelless","keenness"]) = ["keles","kenes"].
+
+Strings will be lowercase only, no spaces. See test cases for more examples.
+
+
+ПЕРЕВОД:
+В этом Ката вам будет дан массив строк, и ваша задача состоит в том, чтобы 
+удалить все последовательные дубликаты букв из каждой строки в массиве.
+
+Например:
+
+dup(["abracadabra", "allottee", "assessee"]) = ["abracadabra", "alote", "asese"].
+
+dup(["kelless", "keenness"]) = ["keles", "kenes"].
+
+Строки будут только в нижнем регистре, без пробелов. Дополнительные примеры см. в тестовых примерах.
+*/
