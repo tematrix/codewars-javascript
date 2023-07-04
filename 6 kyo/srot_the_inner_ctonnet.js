c176f40f716874599eda0d836a95ac5db88a4963
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-07-04 16:27:06 +0300

    Srot the inner ctonnet - translation

--- 6 kyo/srot_the_inner_ctonnet.js
+++ 6 kyo/srot_the_inner_ctonnet.js
@@ -16,4 +16,23 @@ The string will never be null and will never be empty. In C/C++ the string is al
 Have fun coding it and please don't forget to vote and rank this kata! :-)
 
 I have also created other katas. Take a look if you enjoyed this kata!
+
+
+ПЕРЕВОД:
+Вам нужно отсортировать внутреннее содержимое каждого слова строки в порядке убывания.
+
+Внутреннее содержимое - это содержимое слова без первого и последнего символа.
+
+Некоторые примеры:
+
+"sort the inner content in descending order" --> "srot the inner ctonnet in dsnnieedcg oredr"
+"wait for me" --> "wiat for me"
+"эта ката легкая" --> "тихс катаа эсай".
+Слова состоят из строчных букв.
+
+Строка никогда не будет нулевой и никогда не будет пустой. В C/C++ строка всегда nul-terminated.
+
+Получайте удовольствие от кодирования и, пожалуйста, не забудьте проголосовать и поставить оценку этому ката! :-)
+
+Я также создал другие ката. Посмотрите, если вам понравилась эта ката!
 */
