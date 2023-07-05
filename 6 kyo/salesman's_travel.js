89af5362981cb75d0f7f9911b68564e34451b4ff
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-07-05 22:05:10 +0300

    Salesman's travel - translation

--- 6 kyo/salesman's_travel.js
+++ 6 kyo/salesman's_travel.js
@@ -31,9 +31,50 @@ travel(r, "OH 43071") --> "OH 43071:Main Street St. Louisville,Main Long Road St
 travel(r, "NY 56432") --> "NY 56432:High Street Pollocksville/786"
 
 travel(r, "NY 5643") --> "NY 5643:/"
+
 Note for Elixir:
 In Elixir the empty addresses' input is an empty list, not an empty string.
 
 Note:
 You can see a few addresses and zipcodes in the test cases.
+
+
+ПЕРЕВОД:
+Странствующий торговец должен посещать клиентов. Он получил от каждого клиента 
+адрес, например, "432 Main Long Road St. Louisville OH 43071" в виде списка.
+
+Основной формат почтового индекса обычно состоит из двух заглавных букв, за которыми следует 
+пробела и пяти цифр. Список клиентов, которых необходимо посетить, был представлен в виде 
+строка всех адресов, каждый из которых отделен от других запятой, например:
+
+"123 Main Street St. Louisville OH 43071,432 Main Long Road St. Louisville 
+OH 43071,786 High Street Pollocksville NY 56432".
+
+Для облегчения путешествия он хочет сгруппировать список по почтовому индексу.
+
+Задача
+Функция travel будет принимать два параметра r (список адресов всех 
+клиентов в виде строки) и почтовый индекс и возвращает строку в следующем формате:
+
+zipcode:улица и город,улица и город,.../номер дома,номер дома,...
+
+Номера улиц должны быть в том же порядке, что и улицы, к которым они относятся.
+
+Если заданный почтовый индекс не существует в списке адресов клиентов, возвращается "zipcode:/".
+
+Примеры
+r = "123 Main Street St. Louisville OH 43071,432 Main Long Road 
+St. Louisville OH 43071,786 High Street Pollocksville NY 56432"
+
+travel(r, "OH 43071") --> "OH 43071:Main Street St. Louisville,Main Long Road St. Louisville/123,432"
+
+travel(r, "NY 56432") --> "NY 56432:High Street Pollocksville/786"
+
+travel(r, "NY 5643") --> "NY 5643:/".
+
+Примечание для Elixir:
+В Elixir входные данные пустых адресов - это пустой список, а не пустая строка.
+
+Примечание:
+Вы можете увидеть несколько адресов и почтовых индексов в тестовых примерах.
 */
