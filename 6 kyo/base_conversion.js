e88d62bcdfea8653ec9c4cb3c0352186b757b05a
Artem Modelin <108069989+tematrix@users.noreply.github.com>
2023-07-05 22:09:51 +0300

    Base conversion - translation

--- 6 kyo/base_conversion.js
+++ 6 kyo/base_conversion.js
@@ -36,4 +36,43 @@ The maximum input value can always be encoded in a number without loss of precis
 In Haskell, intermediate results will probably be too large for Int.
 The function must work for any arbitrary alphabets, not only the pre-defined ones
 You don't have to consider negative numbers
+
+
+ПЕРЕВОД:
+В этом ката вы должны реализовать конвертер оснований, который преобразует целые положительные числа 
+между произвольными базами / алфавитами. Здесь приведены некоторые предопределенные алфавиты:
+
+var Alphabet = {
+  БИНАРНЫЙ: '01',
+  ВОСЬМЕРИЧНАЯ: '01234567',
+  ДЕСЯТИЧНАЯ: '0123456789',
+  HEXA_DECIMAL: '0123456789abcdef',
+  ALPHA_LOWER: 'abcdefghijklmnopqrstuvwxyz',
+  ALPHA_UPPER: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  ALPHA: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  ALPHA_NUMERIC: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+};
+
+Функция convert() должна принимать входные данные (строка), исходный алфавит (строка) 
+и целевой алфавит (строка). Можно предположить, что входное значение всегда состоит 
+из символов исходного алфавита. Вам не нужно его проверять.
+
+Примеры
+// преобразование между системами счисления
+convert("15", Alphabet.DECIMAL, Alphabet.BINARY); // должно вернуться "1111"
+convert("15", Alphabet.DECIMAL, Alphabet.OCTAL); // должно возвращаться "17"
+convert("1010", Alphabet.BINARY, Alphabet.DECIMAL); // должно вернуться "10"
+convert("1010", Alphabet.BINARY, Alphabet.HEXA_DECIMAL); // должно вернуться "a"
+
+// другие основания
+convert("0", Alphabet.DECIMAL, Alphabet.ALPHA); // должно вернуться "a"
+convert("27", Alphabet.DECIMAL, Alphabet.ALPHA_LOWER); // должно вернуться "bb"
+convert("hello", Alphabet.ALPHA_LOWER, Alphabet.HEXA_DECIMAL); // должно вернуться "320048"
+convert("SAME", Alphabet.ALPHA_UPPER, Alphabet.ALPHA_UPPER); // должно вернуться "SAME"
+Дополнительные примечания:
+
+Максимальное входное значение всегда может быть закодировано в число без потери точности в JavaScript. 
+В Haskell промежуточные результаты, вероятно, будут слишком большими для Int.
+Функция должна работать для любых произвольных алфавитов, а не только для предопределенных.
+Не нужно учитывать отрицательные числа
 */
